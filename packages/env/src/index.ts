e7cea2441d5530ea5a156025368017f109688cee
Shun Ueda <me@shu.nu>
2025-05-24 03:37:39 -0400

    chore: fix access

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -4,22 +4,15 @@ import { parse } from '@dotenvx/dotenvx'
 import { dirs } from './dirs'
 import type { EnvType } from './env-type'
 
-const { ENVIRONMENT, NODE_ENV, DOTENV_PRIVATE_KEY, GITHUB_ENV } = process.env
-
-console.log(process.env)
-
-console.log(ENVIRONMENT)
-console.log(NODE_ENV)
-console.log(ENVIRONMENT || NODE_ENV || 'local')
-
-const environment = ENVIRONMENT || NODE_ENV || 'local'
+const environment = process.env.ENVIRONMENT || process.env.NODE_ENV || 'local'
 
 console.log(`Loading environment variables for ${environment}...`)
 
 const envFile = join(dirs.envs, environment, '.env')
 const envKeysFile = join(dirs.envs, environment, '.env.keys')
 const privateKey =
-  DOTENV_PRIVATE_KEY || parse(await readFile(envKeysFile)).DOTENV_PRIVATE_KEY
+  process.env.DOTENV_PRIVATE_KEY ||
+  parse(await readFile(envKeysFile)).DOTENV_PRIVATE_KEY
 
 const output = parse(await readFile(envFile), {
   privateKey
@@ -32,10 +25,10 @@ export const Env = {
   ...output
 } as EnvType
 
-if (GITHUB_ENV) {
+if (process.env.GITHUB_ENV) {
   const env = Object.entries(Env)
     .map(([key, value]) => `${key}=${value}`)
     .join('\n')
 
-  await writeFile(GITHUB_ENV, env)
+  await writeFile(process.env.GITHUB_ENV, env)
 }
