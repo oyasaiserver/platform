448a8d806cfe3158995e4dfa9086494fdf6a7f13
Shun Ueda <me@shu.nu>
2025-05-24 02:45:29 -0400

    log

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -9,6 +9,8 @@ const environment = process.env.ENVIRONMENT || process.env.NODE_ENV || 'local'
 const envFile = join(dirs.envs, environment, '.env')
 const envKeysFile = join(dirs.envs, environment, '.env.keys')
 
+console.log(process.env.DOTENV_PRIVATE_KEY)
+
 const output = parse(await readFile(envFile), {
   privateKey:
     process.env.DOTENV_PRIVATE_KEY || (await readFile(envKeysFile, 'utf-8'))
