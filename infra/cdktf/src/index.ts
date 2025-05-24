214ae87e1cea725d5a740ede51befd2cb98f23aa
Shun Ueda <me@shu.nu>
2025-05-24 04:46:02 -0400

    ci: explicit credential

--- infra/cdktf/src/index.ts
+++ infra/cdktf/src/index.ts
@@ -5,7 +5,6 @@ import { CloudflareStack } from './stacks/cloudflare-stack'
 export class PlatformApp extends App {
   public constructor() {
     super()
-    process.env.TF_TOKEN_app_terraform_io = Env.TF_API_TOKEN
     new CloudflareStack(this, `cloudflare-${Env.ENVIRONMENT}`)
   }
 }
