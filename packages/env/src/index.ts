da6e7c6f63879ed7690ad39c6ad0b00c24e4fc63
Shun Ueda <me@shu.nu>
2025-05-24 04:15:21 -0400

    ci: use raw env

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -4,32 +4,15 @@ import { config } from '@dotenvx/dotenvx'
 import { dirs } from './dirs'
 import type { EnvType } from './env-type'
 
-console.log(process.env)
-
 const environment = process.env.NODE_ENV || process.env.ENVIRONMENT || 'local'
-
-console.log(`Loading environment variables for ${environment}...`)
 
 const envFile = join(dirs.envs, environment, '.env')
 const envKeysFile = join(dirs.envs, environment, '.env.keys')
 
-if (process.env.DOTENV_PRIVATE_KEY) {
-  console.log('Writing private key to .env.keys file...')
-  await writeFile(
-    envKeysFile,
-    `DOTENV_PRIVATE_KEY="${process.env.DOTENV_PRIVATE_KEY}"`
-  )
-  console.log(
-    `DOTENV_PRIVATE_KEY="${process.env.DOTENV_PRIVATE_KEY.toUpperCase()}"`
-  )
-}
-
 const output = config({
   path: envFile,
-  envKeysFile
+  envKeysFile: process.env.DOTENV_PRIVATE_KEY ? undefined : envKeysFile
 })
-
-console.log(output)
 
 export const Env = {
   ENVIRONMENT: environment,
