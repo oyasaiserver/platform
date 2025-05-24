e05cf9bb10920b4ab98e397ee56db74a3dca7536
Shun Ueda <me@shu.nu>
2025-05-24 03:28:44 -0400

    ci: cdktf no drift (#3)
    * add checks
    
    * fix
    
    * add log
    
    * fix env
    
    * add secrets
    
    * log
    
    * log
    
    * add to env
    
    * fix name
    
    * fix env parsing
    
    * log
    
    * add writer
    
    * add seeder
    
    * log
    
    * log
    
    * use inline
    
    * add cd

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -1,16 +1,35 @@
+import { readFile, writeFile } from 'node:fs/promises'
 import { join } from 'node:path'
-import { config } from '@dotenvx/dotenvx'
+import { parse } from '@dotenvx/dotenvx'
 import { dirs } from './dirs'
 import type { EnvType } from './env-type'
 
-const environment = process.env.ENVIRONMENT || 'local'
+const { ENVIRONMENT, NODE_ENV, DOTENV_PRIVATE_KEY, GITHUB_ENV } = process.env
 
-const output = config({
-  path: join(dirs.envs, environment, '.env'),
-  envKeysFile: join(dirs.envs, environment, '.env.keys')
+const environment = ENVIRONMENT || NODE_ENV || 'local'
+
+console.log(`Loading environment variables for ${environment}...`)
+
+const envFile = join(dirs.envs, environment, '.env')
+const envKeysFile = join(dirs.envs, environment, '.env.keys')
+const privateKey =
+  DOTENV_PRIVATE_KEY || parse(await readFile(envKeysFile)).DOTENV_PRIVATE_KEY
+
+const output = parse(await readFile(envFile), {
+  privateKey
 })
+
+Object.assign(process.env, output)
 
 export const Env = {
   ENVIRONMENT: environment,
-  ...output.parsed
+  ...output
 } as EnvType
+
+if (GITHUB_ENV) {
+  const env = Object.entries(Env)
+    .map(([key, value]) => `${key}=${value}`)
+    .join('\n')
+
+  await writeFile(GITHUB_ENV, env)
+}
