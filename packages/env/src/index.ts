0a44ba39548a8650b26da104ca1abbafd384863a
Shun Ueda <me@shu.nu>
2025-05-24 05:09:34 -0400

    feat(env): explicit environment

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -1,6 +1,6 @@
 import type { EnvType } from './env-type'
 
 export const Env = {
-  ENVIRONMENT: process.env.NODE_ENV || process.env.ENVIRONMENT || 'local',
+  ENVIRONMENT: process.env.ENVIRONMENT || process.env.NODE_ENV || 'local',
   ...process.env
 } as EnvType
