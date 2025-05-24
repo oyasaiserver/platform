4e20d034b076aca3d4dc35186906b0d674407936
Shun Ueda <me@shu.nu>
2025-05-24 04:27:45 -0400

    ci: use dotenvx

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -1,28 +1,6 @@
-import { writeFile } from 'node:fs/promises'
-import { join } from 'node:path'
-import { config } from '@dotenvx/dotenvx'
-import { dirs } from './dirs'
 import type { EnvType } from './env-type'
 
-const environment = process.env.NODE_ENV || process.env.ENVIRONMENT || 'local'
-
-const envFile = join(dirs.envs, environment, '.env')
-const envKeysFile = join(dirs.envs, environment, '.env.keys')
-
-const output = config({
-  path: envFile,
-  envKeysFile: process.env.DOTENV_PRIVATE_KEY ? undefined : envKeysFile
-})
-
 export const Env = {
-  ENVIRONMENT: environment,
-  ...output.parsed
+  ENVIRONMENT: process.env.NODE_ENV || process.env.ENVIRONMENT || 'local',
+  ...process.env
 } as EnvType
-
-if (process.env.GITHUB_ENV) {
-  const env = Object.entries(Env)
-    .map(([key, value]) => `${key}=${value}`)
-    .join('\n')
-
-  await writeFile(process.env.GITHUB_ENV, env)
-}
