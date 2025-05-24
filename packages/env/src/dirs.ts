22b41afdf0f4154ba28282eff2f0e60763cfb09c
Shun Ueda <me@shu.nu>
2025-05-24 05:28:57 -0400

    ci: codegen (#4)
    * add drift check
    
    * fix
    
    * should format after
    
    * cleanup
    
    * cleanup
    
    * regen

--- packages/env/src/dirs.ts
+++
@@ -1,7 +0,0 @@
-import { join } from 'node:path'
-import { REPOSITORY_ROOT } from '@platform/constants'
-
-export const dirs = {
-  src: join(import.meta.dirname, '..', 'src'),
-  envs: join(REPOSITORY_ROOT, 'envs')
-} as const
