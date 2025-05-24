11ea1dbaab8543d059ab5b12b0fa32a92eff21e1
Shun Ueda <me@shu.nu>
2025-05-24 04:04:54 -0400

    ci: add quotes

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -17,7 +17,7 @@ const envKeysFile = join(dirs.envs, environment, '.env.keys')
 if (process.env.DOTENV_PRIVATE_KEY) {
   await writeFile(
     envKeysFile,
-    `DOTENV_PRIVATE_KEY=${process.env.DOTENV_PRIVATE_KEY}${EOL}`
+    `DOTENV_PRIVATE_KEY="${process.env.DOTENV_PRIVATE_KEY}"${EOL}`
   )
 }
 
