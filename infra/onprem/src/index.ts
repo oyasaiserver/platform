391c7bbe6e818a2e1027bdd28c71c27dfa3c70d8
Shun Ueda <me@shu.nu>
2025-05-24 23:07:08 -0400

    cleanup

--- infra/onprem/src/index.ts
+++ infra/onprem/src/index.ts
@@ -1,25 +1,26 @@
 #!/usr/bin/env -S npx tsx
-import { cp } from 'node:fs/promises'
+import { copyFile } from 'node:fs/promises'
 import { join, resolve } from 'node:path'
 import { cwd } from 'node:process'
 import { $ } from 'zx'
 
 $.verbose = true
-$.nothrow = true
 
-const assetsDir = resolve(import.meta.dirname, '../assets')
+// assets directory
+const assets = resolve(import.meta.dirname, '../assets')
 
 // update compose.yaml
-await cp(join(assetsDir, 'compose.yaml'), join(cwd(), 'compose.yaml'), {
-  force: true
-})
+const composeYaml = 'compose.yaml'
+await copyFile(join(assets, composeYaml), join(cwd(), composeYaml))
 
-//
-// // copy the compose.yaml
-// await writeFile('compose.yaml', await readFile(composeYaml, 'utf-8'))
-//
-// // Stop the containers if they are running
-// await $`docker compose --profile production --profile development -f compose.yaml down --remove-orphans`
-//
-// // Start the containers
-// await $`docker compose --profile production --profile development -f compose.yaml up -d --wait`
+// docker compose command
+const docker =
+  'docker compose --profile production --profile development -f compose.yaml down'
+
+// Stop the containers if they are running
+await $`${docker} down --remove-orphans`
+
+// backup the world data
+
+// Start the containers
+await $`${docker} up -d --wait`
