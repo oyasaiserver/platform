ead80e77871034fae769daf2bf5bc82c465972c9
Shun Ueda <me@shu.nu>
2025-05-25 02:21:18 -0400

    add log

--- infra/onprem/src/services/assets.ts
+++ infra/onprem/src/services/assets.ts
@@ -9,6 +9,9 @@ export class Assets {
   )
 
   public static async clone(filename: string, destination = ''): Promise<void> {
+    console.log(
+      `Copying ${filename} from ${join(Assets.path, filename)} to ${join(cwd(), destination, filename)}`
+    )
     await copyFile(
       join(Assets.path, filename),
       join(cwd(), destination, filename)
