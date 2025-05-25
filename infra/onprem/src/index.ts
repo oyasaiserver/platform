c4186c3ef241cd8a729f5e6d90165b0cccfc80a5
Shun Ueda <me@shu.nu>
2025-05-25 12:05:38 -0400

    rename

--- infra/onprem/src/index.ts
+++ infra/onprem/src/index.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S npx tsx
 import { rm } from 'node:fs/promises'
-import task from 'tasuku'
+import step from 'tasuku'
 import { Assets } from './services/assets'
 import { Backup } from './services/backup'
 import { DockerCompose } from './services/docker-compose'
@@ -8,11 +8,11 @@ import { Overlays } from './services/overlays'
 
 const environment = process.argv.at(2) || 'local'
 
-await task('docker-compose-down', async () => {
+await step('docker-compose-down', async () => {
   await DockerCompose.down(environment)
 })
 
-await task('backup-clean-and-restore', async () => {
+await step('backup-clean-and-restore', async () => {
   const backup = await Backup.create(`${environment}/minecraft-main/worlds`)
   await rm(environment, {
     recursive: true,
@@ -21,14 +21,14 @@ await task('backup-clean-and-restore', async () => {
   await backup?.restore()
 })
 
-await task('apply-overlays', async () => {
+await step('apply-overlays', async () => {
   await Overlays.apply(`${Assets.path}/overlays`, environment)
 })
 
-await task('clone-compose-yaml', async () => {
+await step('clone-compose-yaml', async () => {
   await Assets.clone('compose.yaml')
 })
 
-await task('docker-compose-up', async () => {
+await step('docker-compose-up', async () => {
   await DockerCompose.up(environment)
 })
