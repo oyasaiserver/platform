0e8c6b688e6fb2d42b3c29401dd17a5ddac8d4db
Shun Ueda <me@shu.nu>
2025-05-25 21:33:38 -0400

    cleanup

--- plugins/Vertex/src/main/kotlin/com/github/oyasaiserver/vertex/database/DatabaseManager.kt
+++ plugins/Vertex/src/main/kotlin/com/github/oyasaiserver/vertex/database/DatabaseManager.kt
@@ -1,40 +1,40 @@
 package com.github.oyasaiserver.vertex.database
 
 object DatabaseManager {
-//    val postgres by lazy {
-//        Database.connect(
-//            url = "jdbc:postgresql://${Postgres.HOST}:${Postgres.PORT}/${Postgres.DB}",
-//            user = Postgres.USER,
-//            password = Postgres.PASSWORD,
-//        )
-//    }
+    //    val postgres by lazy {
+    //        Database.connect(
+    //            url = "jdbc:postgresql://${Postgres.HOST}:${Postgres.PORT}/${Postgres.DB}",
+    //            user = Postgres.USER,
+    //            password = Postgres.PASSWORD,
+    //        )
+    //    }
 
     fun initialize() {
-//        Database
-//            .connect(
-//                url = "jdbc:postgresql://${Postgres.HOST}:${Postgres.PORT}/postgres",
-//                user = Postgres.USER,
-//                password = Postgres.PASSWORD,
-//            ).also {
-//                transaction(it) {
-//                    connection.autoCommit = true
-//                    if (Postgres.DB !in SchemaUtils.listDatabases()) {
-//                        SchemaUtils.createDatabase(Postgres.DB)
-//                    }
-//                    connection.autoCommit = false
-//                }
-//                TransactionManager.closeAndUnregister(it)
-//            }
-//        TransactionManager.defaultDatabase = postgres
-//        transaction {
-//            MigrationUtils
-//                .statementsRequiredForDatabaseMigration(
-//                    *reflections.getSubKotlinObjectsOf<Table>().toTypedArray(),
-//                ).forEach { runCatching { exec(it) } }
-//        }
+        //        Database
+        //            .connect(
+        //                url = "jdbc:postgresql://${Postgres.HOST}:${Postgres.PORT}/postgres",
+        //                user = Postgres.USER,
+        //                password = Postgres.PASSWORD,
+        //            ).also {
+        //                transaction(it) {
+        //                    connection.autoCommit = true
+        //                    if (Postgres.DB !in SchemaUtils.listDatabases()) {
+        //                        SchemaUtils.createDatabase(Postgres.DB)
+        //                    }
+        //                    connection.autoCommit = false
+        //                }
+        //                TransactionManager.closeAndUnregister(it)
+        //            }
+        //        TransactionManager.defaultDatabase = postgres
+        //        transaction {
+        //            MigrationUtils
+        //                .statementsRequiredForDatabaseMigration(
+        //                    *reflections.getSubKotlinObjectsOf<Table>().toTypedArray(),
+        //                ).forEach { runCatching { exec(it) } }
+        //        }
     }
 
     fun close() {
-//        TransactionManager.closeAndUnregister(postgres)
+        //        TransactionManager.closeAndUnregister(postgres)
     }
 }
