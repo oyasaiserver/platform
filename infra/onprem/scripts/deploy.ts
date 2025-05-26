6994c3e171ff914fe424228839301ec3adf57032
Shun Ueda <me@shu.nu>
2025-05-25 22:42:42 -0400

    add remove stale

--- infra/onprem/scripts/deploy.ts
+++ infra/onprem/scripts/deploy.ts
@@ -1,7 +1,3 @@
 import { $ as zx$ } from 'zx'
 
 const sha = (await zx$`git rev-parse --short HEAD`).text().trim()
-
-console.log(sha)
-
-// ssh into onprem & run npx
--- infra/onprem/src/services/backup.ts
+++ infra/onprem/src/services/backup.ts
@@ -1,43 +1,57 @@
-import { mkdir, stat } from 'node:fs/promises'
-import { basename, dirname, join } from 'node:path'
+import { mkdir, readdir, rm, stat } from 'node:fs/promises'
+import { basename, dirname, join, resolve } from 'node:path'
 import { $ } from 'zx'
 
 $.quiet = true
 
 export class Backup {
-  private static readonly directory: string = 'backups'
-
-  public constructor(
+  private constructor(
+    private readonly name: string,
     private readonly archive: string,
-    private readonly original: string
+    private readonly original: string,
+    private readonly backupDir: string
   ) {}
 
-  public static async create(dir: string): Promise<Backup | undefined> {
+  public static async create(
+    dir: string,
+    backupDir: string
+  ): Promise<Backup | undefined> {
     try {
       if (!(await stat(dir)).isDirectory()) {
         return
       }
-    } catch (_) {
+    } catch {
       return
     }
 
-    await mkdir(Backup.directory, {
+    const directory = resolve(backupDir)
+    await mkdir(directory, {
       recursive: true
     })
 
     const name = basename(dir)
-    const base = join(dir, '..')
-    const archive = join(Backup.directory, `${name}-${Date.now()}.tar.zst`)
+    const base = dirname(dir)
+    const archive = join(directory, `${name}-${Date.now()}.tar.zst`)
 
     await $`tar -C ${base} -cf - ${name} | zstd -T0 -5 -o ${archive}`
-    return new Backup(archive, dir)
+    return new Backup(name, archive, dir, backupDir)
+  }
+
+  public async removeStale(): Promise<void> {
+    const archiveName = basename(this.archive)
+    for (const file of await readdir(this.backupDir)) {
+      if (file.startsWith(this.name) && file !== archiveName) {
+        await rm(`${this.backupDir}/${file}`, {
+          force: true,
+          recursive: true
+        })
+      }
+    }
   }
 
   public async restore(): Promise<void> {
     const restoreTo = dirname(this.original)
-    await mkdir(restoreTo, {
-      recursive: true
-    })
+    await mkdir(restoreTo, { recursive: true })
     await $`zstd -d -c ${this.archive} | tar -xf - -C ${restoreTo}`
   }
 }
