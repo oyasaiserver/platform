daf9fe73113b24bdc03b23f3a80763dfdd464487
Shun Ueda <me@shu.nu>
2025-05-30 21:44:41 -0400

    mpl

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,21 @@
 
 brew install asdf
 
+# shellcheck disable=SC2016
+LINE='export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"'
+
+# Determine which shell rc file to use
+case "$(basename "$SHELL")" in
+  zsh) rcfile="$HOME/.zshrc" ;;
+  bash) rcfile="$HOME/.bashrc" ;;
+  *) echo "Unsupported shell: $SHELL" >&2; exit 1 ;;
+esac
+
+# Add the line only if it's not already there
+if ! grep -Fxq "$LINE" "$rcfile"; then
+  printf '\n%s\n' "$LINE" >> "$rcfile"
+fi
+
 # install nodejs plugin
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 
