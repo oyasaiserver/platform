eff019791f2ddc1d97778f6e01ca5370203a6588
Shun Ueda <me@shu.nu>
2025-06-03 22:32:26 -0400

    build: add bundle to core (#32)

--- packages/core/src/services/env.ts
+++ packages/core/src/services/env.ts
@@ -1,24 +1,14 @@
-import { join, resolve } from 'node:path'
-import { type DotenvPopulateInput, config } from '@dotenvx/dotenvx'
+import { parse } from '@dotenvx/dotenvx'
+import type { EnvType } from '@oyasaiserver/env'
 
 /**
  * Can't use `@oyasaiserver/env` - remember, this is a standalone cli application.
  * `.env` files are embedded to the package root during publishing.
  */
 export class Env {
-  private static readonly path: string = resolve(
-    import.meta.dirname,
-    '../../envs'
-  )
-
-  public static parse(environemnt = 'local'): DotenvPopulateInput {
-    const processEnv: DotenvPopulateInput = {}
-    config({
-      strict: true,
-      quiet: true,
-      processEnv,
-      path: join(Env.path, environemnt, '.env')
-    })
-    return processEnv
+  public static async fetch(): Promise<EnvType> {
+    return parse(process.env.DOTENV_PUBLIC_KEYS as string, {
+      privateKey: process.env.DOTENV_PRIVATE_KEY
+    }) as EnvType
   }
 }
