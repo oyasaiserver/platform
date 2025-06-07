122d923b93a098fcfbeef886bae3c15a58ceed1b
Shun Ueda <me@shu.nu>
2025-06-07 00:33:40 -0400

    fix(ci): env

--- packages/platform/src/env.ts
+++ packages/platform/src/env.ts
@@ -1,17 +1,17 @@
-import { readFile } from 'node:fs/promises'
 import { join } from 'node:path'
-import { parse } from '@dotenvx/dotenvx'
+import { config } from '@dotenvx/dotenvx'
 import { env } from '@oyasaiserver/gen/common/env'
 import { fallback } from './utils'
 
 const environment = fallback(process.env.ENVIRONMENT, 'local')
 
-const path = join('../..', 'envs', environment, '.env')
-const content = await readFile(path)
+config({
+  strict: true,
+  override: true,
+  path: join('../..', 'envs', environment, '.env')
+})
 
 export const Env = env.parse({
   ENVIRONMENT: environment,
-  ...parse(content, {
-    privateKey: process.env.DOTENV_PRIVATE_KEY
-  })
+  ...process.env
 })
