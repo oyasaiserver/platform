d883d33217e985cc55fda7e998a753fd3dba7620
Shun Ueda <me@shu.nu>
2025-06-08 15:48:09 -0400

    remove check

--- packages/codegen/src/index.ts
+++ packages/codegen/src/index.ts
@@ -4,6 +4,7 @@ import { join } from 'node:path'
 import { argv } from 'node:process'
 import { directory } from '@oyasaiserver/platform/directory'
 import { ensure } from '@oyasaiserver/platform/utils'
+import { spinner } from 'zx'
 import { json } from './generators/json.ts'
 import { ts } from './generators/ts.ts'
 
@@ -13,26 +14,31 @@ const dst = join(directory.root, ensure(argv[3]))
 console.log('Source directory:', src)
 console.log('Destination directory:', dst)
 
-await rm(`dst`, {
-  force: true,
-  recursive: true
+await spinner('reset', async () => {
+  await rm(`dst`, {
+    force: true,
+    recursive: true
+  })
+  await mkdir(dst, {
+    recursive: true
+  })
+  await json(dst)
 })
-await mkdir(dst, {
-  recursive: true
-})
-await json(dst)
+
 console.log('Generating TypeScript files from JSON schemas...')
 
-const paths = await readdir(src, {
-  recursive: true
-})
-console.log('Found JSON schema files:', paths)
-const promises = paths
-  .filter(path => path.endsWith('.json'))
-  .map(async path => {
-    console.log('Processing file:', path)
-    await ts(path, src, dst)
+await spinner('generate', async () => {
+  const paths = await readdir(src, {
+    recursive: true
   })
-console.log('Total files to process:', promises.length)
-await Promise.all(promises)
-console.log('TypeScript files generated successfully.')
+  console.log('Found JSON schema files:', paths)
+  const promises = paths
+    .filter(path => path.endsWith('.json'))
+    .map(async path => {
+      console.log('Processing file:', path)
+      await ts(path, src, dst)
+    })
+  console.log('Total files to process:', promises.length)
+  await Promise.all(promises)
+  console.log('TypeScript files generated successfully.')
+})
