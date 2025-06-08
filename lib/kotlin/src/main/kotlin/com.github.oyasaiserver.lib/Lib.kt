4d0f0c324c1f3c55e75986bb927b8f8a98df8d68
Shun Ueda <me@shu.nu>
2025-06-08 17:51:51 -0400

    impl

---
+++ lib/kotlin/src/main/kotlin/com.github.oyasaiserver.lib/Lib.kt
@@ -0,0 +1,3 @@
+package com.github.oyasaiserver.lib
+
+object Lib
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,14 @@
+plugins {
+    id("com.gradleup.shadow") version "9.0.0-beta12" apply false
+    kotlin("jvm") version "2.2.0-Beta2" apply false
+}
+
 rootProject.name = "platform"
 
-"plugins".let {
-    file(it)
-        .listFiles()
-        .filter { it.isDirectory }
-        .forEach { plugin ->
-            include("$it:${plugin.name}")
-        }
+"plugins".let { dir ->
+    file(dir).listFiles().filter { it.isDirectory }.forEach { plugin ->
+        include("$dir:${plugin.name}")
+    }
 }
+
+include(":lib:kotlin")
