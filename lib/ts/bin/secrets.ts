0d5de5b6bf0e76013402efbad45d1c7f89ad0c06
Shun Ueda <me@shu.nu>
2025-06-14 15:01:53 -0400

    refactor: extract codegen (#51)

--- lib/ts/bin/secrets.ts
+++ lib/ts/bin/secrets.ts
@@ -2,12 +2,10 @@
 import { $ } from 'zx'
 import { secrets } from '../src/secrets.ts'
 
-const command = process.argv.slice(2)
-
 $({
   env: {
     ...process.env,
     ...secrets
   },
   verbose: true
-})`${command}`
+})`${process.argv.slice(2)}`
