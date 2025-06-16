7dea5e17c2ccfb5149eaaa936d77608be8a8e29b
Shun Ueda <me@shu.nu>
2025-06-16 09:07:50 -0400

    cleanup

--- lib/ts/src/dedent.ts
+++ lib/ts/src/dedent.ts
@@ -1,6 +1,37 @@
-/**
- * TODO implement and remove dependency
- */
-import dedent from 'dedent'
+import { EOL } from 'node:os'
 
-export { dedent }
+export function dedent(literals: string): string
+export function dedent(
+  strings: TemplateStringsArray,
+  ...values: unknown[]
+): string
+export function dedent(
+  strings: TemplateStringsArray | string,
+  ...values: unknown[]
+): string {
+  const result = (typeof strings === 'string' ? [strings] : strings.raw).reduce(
+    (acc, str, i) => acc + str + (i < values.length ? values[i] : ''),
+    ''
+  )
+
+  const lines = result.split(EOL)
+
+  const mindent = lines
+    .filter(line => line.trim())
+    .reduce<number | null>((min, line) => {
+      const length = line.match(/^(\s+)\S+/)?.at(1)?.length
+      if (!length) {
+        return min
+      }
+      return min === null ? length : Math.min(min, length)
+    }, null)
+
+  const dedented = mindent
+    ? lines.map(line =>
+        line.startsWith(' ') || line.startsWith('\t')
+          ? line.slice(mindent)
+          : line
+      )
+    : lines
+  return dedented.join(EOL).trim()
+}
