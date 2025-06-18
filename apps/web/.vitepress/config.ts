d148d9b6913afab2e0ed8f56e4e0678bbafd2a4e
Shun Ueda <me@shu.nu>
2025-06-17 20:57:48 -0400

    cleanup

--- apps/web/.vitepress/config.ts
+++ apps/web/.vitepress/config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  srcDir: 'pages',
+  srcDir: 'src/pages',
   title: 'web',
   description: 'A VitePress Site',
   themeConfig: {
