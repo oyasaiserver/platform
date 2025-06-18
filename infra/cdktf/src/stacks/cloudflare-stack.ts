9aa506b41926b51b3853dd4301f0aec3287f79f3
Shun Ueda <me@shu.nu>
2025-06-17 23:18:38 -0400

    fmt

--- infra/cdktf/src/stacks/cloudflare-stack.ts
+++ infra/cdktf/src/stacks/cloudflare-stack.ts
@@ -1,11 +1,11 @@
+import { DnsRecord } from '@cdktf/provider-cloudflare/lib/dns-record'
 import { CloudflareProvider } from '@cdktf/provider-cloudflare/lib/provider'
+import { WorkersRoute } from '@cdktf/provider-cloudflare/lib/workers-route'
 import { secrets } from '@oyasaiserver/lib/secrets'
 import { TerraformStack } from 'cdktf'
 import type { Construct } from 'constructs'
-import { NamedCloudBackend } from '../backend/named-cloud-backend.ts'
-import { DnsRecord } from "@cdktf/provider-cloudflare/lib/dns-record";
-import { WorkersRoute } from "@cdktf/provider-cloudflare/lib/workers-route";
 import wikiWrangerJson from '../../../../apps/wiki/wrangler.json'
+import { NamedCloudBackend } from '../backend/named-cloud-backend.ts'
 
 export class CloudflareStack extends TerraformStack {
   private readonly wikiOyasaiIoZoneId = '3a06bb11a935fe62b10f7ee4a312e85d'
@@ -27,12 +27,12 @@ export class CloudflareStack extends TerraformStack {
       type: 'A',
       proxied: true,
       content: this.dummyIp
-    });
+    })
 
     new WorkersRoute(this, 'wiki-workers-route', {
       zoneId: this.wikiOyasaiIoZoneId,
       pattern: wikiWrangerJson.route,
       script: wikiWrangerJson.name
-    });
+    })
   }
 }
