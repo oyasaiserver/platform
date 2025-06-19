1d9be8e3283ef6251c504629b6c85be7e7fd7c3d
Shun Ueda <me@shu.nu>
2025-06-19 09:39:27 -0400

    refactor(lib): centralize repo info (#70)

--- apps/wiki/src/index.ts
+++ apps/wiki/src/index.ts
@@ -1,7 +1,7 @@
 import { WorkerEntrypoint } from 'cloudflare:workers'
 
 export default class extends WorkerEntrypoint<Env> {
-  override fetch(request: Request) {
+  override async fetch(request: Request) {
     return this.env.assets.fetch(request)
   }
 }
