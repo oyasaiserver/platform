83d96d00c8611d4c6cd875d2d109ff1fa4b594a6
Shun Ueda <me@shu.nu>
2025-06-21 12:38:46 -0400

    impl

--- gen/kotlin/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins { kotlin("jvm") }
-
-repositories { mavenCentral() }
-
-dependencies {
-    implementation(platform(libs.serialization.bom))
-    implementation(libs.serialization.core)
-    implementation(libs.serialization.json)
-}
--- packages/codegen/src/index.ts
+++ packages/codegen/src/index.ts
@@ -1,68 +1,67 @@
-#!/usr/bin/env tsx
-import { readdir, rm } from 'node:fs/promises'
-import { join, parse } from 'node:path'
-import { readme } from '@oyasaiserver/assets/readme.tsx'
-import { directory } from '@oyasaiserver/lib/directory'
-import { readFileJson, writeFileSafe } from '@oyasaiserver/lib/fs'
-import { ensure } from '@oyasaiserver/lib/utils'
-import { $, argv, spinner } from '@oyasaiserver/lib/zx'
-import { pascalCase } from 'change-case'
-import type { JsonSchema } from 'json-schema-to-zod'
-import { gradle } from './generators/gradle.ts'
-import { json } from './generators/json.ts'
-import { kotlin } from './generators/kotlin.ts'
-import { md } from './generators/md.ts'
-import { ts } from './generators/ts.ts'
+import { rm } from 'node:fs/promises'
+import { writeFileSafe } from '@oyasaiserver/lib/fs'
+import { renderToString } from '@oyasaiserver/lib/preact'
+import { readme } from '../assets/readme.tsx'
 
-const src = join(directory.root, ensure(argv.src))
-const out = join(directory.root, ensure(argv.out))
+const src = 'schema'
+const out = 'gen'
 
-await spinner('reset', async () => {
-  await rm(out, {
-    force: true,
-    recursive: true
-  })
+await rm(out, {
+  recursive: true,
+  force: true
 })
 
-await spinner('generate', async () => {
-  await json(`${out}/ts`)
-  await gradle(`${out}/kotlin`)
-  const paths = await readdir(src, {
-    recursive: true
-  })
-  const promises = paths
-    .filter(path => path.endsWith('.json'))
-    .map(async path => {
-      const { name, dir } = parse(path)
-      const schema = await readFileJson<JsonSchema>(`${src}/${path}`)
-      await writeFileSafe(
-        `${out}/ts/src/${argv.src}/${dir}/${name}.ts`,
-        await ts({
-          schema,
-          name
-        })
-      )
-      await writeFileSafe(
-        `${out}/kotlin/src/main/kotlin/io/oyasai/gen/${argv.src}/${dir}/${pascalCase(name)}.kt`,
-        await kotlin({
-          schema,
-          name,
-          dir,
-          src: argv.src
-        })
-      )
-    })
-  await Promise.all(promises)
-  await md({
-    dir: `${out}/md`,
-    component: readme,
-    filename: 'README.md'
-  })
-})
+await writeFileSafe(`${out}/md/README.md`, renderToString(readme))
 
-await spinner('format', async () => {
-  await $({
-    cwd: directory.root,
-    verbose: true
-  })`npm run check && ./gradlew spotlessApply`
-})
+// const src = join(directory.root, ensure(argv.src))
+// const out = join(directory.root, ensure(argv.out))
+//
+// await spinner('reset', async () => {
+//   await rm(out, {
+//     force: true,
+//     recursive: true
+//   })
+// })
+//
+// await spinner('generate', async () => {
+//   await json(`${out}/ts`)
+//   await gradle(`${out}/kotlin`)
+//   const paths = await readdir(src, {
+//     recursive: true
+//   })
+//   const promises = paths
+//     .filter(path => path.endsWith('.json'))
+//     .map(async path => {
+//       const { name, dir } = parse(path)
+//       const schema = await readFileJson<JsonSchema>(`${src}/${path}`)
+//       await writeFileSafe(
+//         `${out}/ts/src/${argv.src}/${dir}/${name}.ts`,
+//         await ts({
+//           schema,
+//           name
+//         })
+//       )
+//       await writeFileSafe(
+//         `${out}/kotlin/src/main/kotlin/io/oyasai/gen/${argv.src}/${dir}/${pascalCase(name)}.kt`,
+//         await kotlin({
+//           schema,
+//           name,
+//           dir,
+//           src: argv.src
+//         })
+//       )
+//     })
+//   await Promise.all(promises)
+//   await md({
+//     dir: `${out}/md`,
+//     component: readme,
+//     filename: 'README.md'
+//   })
+// })
+//
+// await spinner('format', async () => {
+//   await $({
+//     cwd: directory.root,
+//     verbose: true
+//   })`npm run check && ./gradlew spotlessApply`
+// })
