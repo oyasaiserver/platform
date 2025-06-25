9d25a70f6789d771feb3973835e3eac2f98bd56a
Shun Ueda <me@shu.nu>
2025-06-25 08:38:18 -0400

    fix

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -40,16 +40,15 @@ async function main() {
   })
 
   await spinner('upnp-create-mapping', async () => {
-    try {
-      const client = new UpnpClient()
-      await client.createMapping({
-        private: 25565,
-        public: 25565
-      })
-      client.close()
-    } catch {
-      console.warn('UPnP mapping failed')
+    if (environment === 'local') {
+      return
     }
+    const client = new UpnpClient()
+    await client.createMapping({
+      private: 25565,
+      public: 25565
+    })
+    client.close()
   })
 }
 
