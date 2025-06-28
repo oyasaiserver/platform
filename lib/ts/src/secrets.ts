95aca8da6c57148aef61e0e82309d1679b841333
Shun Ueda <me@shu.nu>
2025-06-28 10:04:28 -0400

    cleanup

--- lib/ts/src/secrets.ts
+++ lib/ts/src/secrets.ts
@@ -1,9 +1,27 @@
 import { parseEnv } from 'node:util'
 import { parse } from '@dotenvx/dotenvx'
-import { secrets as secretsSchema } from '@oyasaiserver/gen/json/secrets'
+import { z } from 'zod'
 import { directory } from './directory.ts'
 import { readFileContent } from './fs.ts'
 import { ensure, fallback } from './utils.ts'
+
+// TODO move to codegen
+export const secretsSchema = z
+  .object({
+    DOTENV_PUBLIC_KEY: z.string(),
+    ENVIRONMENT: z.enum(['production', 'development', 'local']),
+    CLOUDFLARE_ACCOUNT_ID: z.string(),
+    CLOUDFLARE_API_TOKEN: z.string(),
+    GITHUB_APP_ID: z.string(),
+    GITHUB_APP_INSTALLATION_ID: z.string(),
+    GITHUB_APP_PEM_FILE: z.string(),
+    TF_API_TOKEN: z.string(),
+    TF_TOKEN_app_terraform_io: z.string()
+  })
+  .strict()
+  .readonly()
+
+export type Secrets = z.infer<typeof secretsSchema>
 
 const envfile = `${directory.root}/envs/${fallback(process.env.ENVIRONMENT, 'local')}/.env`
 
