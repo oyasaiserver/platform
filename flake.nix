2a639cd31d9e2feced2b65a24703eca4c08b5be2
Shun Ueda <me@shu.nu>
2025-06-30 22:32:19 -0400

    impl

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "platform-flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs =with pkgs;  [
+            nodejs_24
+            jdk24_headless
+            terraform
+          ];
+        };
+      });
+}
