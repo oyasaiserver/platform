2dc3748887ff84dbe39c121d0922d51e50bcd816
Shun Ueda <me@shu.nu>
2025-07-01 22:39:06 -0400

    impl

--- apps/sociallikes/src/index.ts
+++ apps/sociallikes/src/index.ts
@@ -1,7 +1,7 @@
-import { Hono } from 'hono'
+import { WorkerEntrypoint } from 'cloudflare:workers'
 
-const app = new Hono()
-
-app.get('/', c => c.text('Hello Cloudflare Workers!'))
-
-export default app
+export default class extends WorkerEntrypoint<Env> {
+  override async fetch(request: Request) {
+    return new Response('SocialLikes')
+  }
+}
