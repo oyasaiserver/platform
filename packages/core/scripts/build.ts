c48c8ef2a89a4a72a54b15fa6f1b73949ffafeaf
Shun Ueda <me@shu.nu>
2025-07-06 14:16:05 -0400

    feat(core): static environment in sea (#114)

--- packages/core/scripts/build.ts
+++ packages/core/scripts/build.ts
@@ -1,38 +1,52 @@
 import { cp, readdir, readFile, rm } from 'node:fs/promises'
 import { join } from 'node:path'
+import { nodeSeaConfig } from '@oyasaiserver/json/store/node-sea-config'
+import { directory } from '@oyasaiserver/lib/directory'
 import { writeJsonFile } from '@oyasaiserver/lib/fs'
+import { secrets } from '@oyasaiserver/lib/secrets'
 import { inject } from 'postject'
 import { build } from 'tsdown'
 import { $, spinner } from 'zx'
 import { bin, config } from '../package.json'
 
 await build({
-  clean: true,
-  noExternal: [/.*/],
   format: ['cjs'],
-  minify: true
+  minify: true,
+  env: {
+    ENVIRONMENT: secrets.ENVIRONMENT
+  }
 })
 
-const seaConfig: SeaConfig = {
+const seaConfig = nodeSeaConfig.parse({
   main: 'dist/index.cjs',
   output: 'dist/sea.blob',
-  disableExperimentalSEAWarning: true,
-  assets: {}
-}
+  disableExperimentalSEAWarning: true
+})
 
-await spinner('create sea config', async () => {
-  const assets = 'assets'
-  const dirents = await readdir(assets, {
+await spinner('copy-infra', async () => {
+  await cp(
+    `${directory.root}/gen/compose/compose.${secrets.ENVIRONMENT}.yaml`,
+    `dist/compose.yaml`
+  )
+})
+
+await spinner('create-sea-config', async () => {
+  const dir = 'assets'
+  const dirents = await readdir(dir, {
     recursive: true,
     withFileTypes: true
   })
-  seaConfig.assets = Object.fromEntries(
+  const assets = Object.fromEntries(
     dirents
       .filter(dirent => dirent.isFile())
       .map(dirent => join(dirent.parentPath, dirent.name))
-      .map(path => [path.substring(assets.length + 1), path])
+      .map(path => [path.substring(dir.length + 1), path])
   )
-  seaConfig.assets[config.sea] = config.sea
+  seaConfig.assets = {
+    ...assets,
+    ['compose.yaml']: 'dist/compose.yaml',
+    [config.sea]: config.sea
+  }
 })
 
 await spinner('compile', async () => {
@@ -42,14 +56,16 @@ await spinner('compile', async () => {
   await rm(config.sea)
 })
 
-const isDarwin = process.platform === 'darwin'
+await spinner('inject', async () => {
+  const isDarwin = process.platform === 'darwin'
 
-await inject(bin, 'NODE_SEA_BLOB', await readFile(seaConfig.output), {
-  sentinelFuse: 'NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2',
-  machoSegmentName: isDarwin ? 'NODE_SEA' : undefined,
-  overwrite: true
+  await inject(bin, 'NODE_SEA_BLOB', await readFile(seaConfig.output), {
+    sentinelFuse: 'NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2',
+    machoSegmentName: isDarwin ? 'NODE_SEA' : undefined,
+    overwrite: true
+  })
+
+  if (isDarwin) {
+    await $`codesign --force --sign - ${bin}`
+  }
 })
-
-if (isDarwin) {
-  await $`codesign --force --sign - ${bin}`
-}
--- packages/core/src/services/file.ts
+++
@@ -1,6 +0,0 @@
-// package.json#files
-type Asset = 'dist' | 'overlays' | 'compose.yaml'
-
-export class File {
-  static async copy(...assets: Asset[]) {}
-}
