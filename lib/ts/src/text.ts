a3f690005691bcfc90dcf8b1ad8123b43af74df5
Shun Ueda <me@shu.nu>
2025-07-09 22:33:15 -0400

    impl

--- lib/ts/src/text.ts
+++
@@ -1,9 +0,0 @@
-export const encoder = new TextEncoder()
-export function encode(...args: Parameters<typeof encoder.encode>): Uint8Array {
-  return encoder.encode(...args)
-}
-
-export const decoder = new TextDecoder()
-export function decode(...args: Parameters<typeof decoder.decode>): string {
-  return decoder.decode(...args)
-}
