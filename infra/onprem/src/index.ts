fcd4f66086a56433dac96e1008f34cf6e581c47c
Shun Ueda <me@shu.nu>
2025-07-10 08:50:14 -0400

    feat(caddy): init (#131)

--- infra/onprem/src/index.ts
+++ infra/onprem/src/index.ts
@@ -1,9 +1,39 @@
 import { defineInfra } from '../lib/define-infra.ts'
+import { envShort } from '@oyasaiserver/lib/environments'
+import dedent from 'dedent'
 
-export default defineInfra(environment => {
+export default defineInfra(async environment => {
   const isLocal = environment === 'local'
+  const redirect = {
+    production: 'https://wiki.oyasai.io',
+    development: 'http://wiki.dev.oyasai.io',
+    local: ''
+  }[environment]
   return {
     services: {
+      ...(!isLocal && {
+        caddy: {
+          image: 'caddy:latest',
+          restart: 'unless-stopped',
+          ports: ['80:80', '443:443'],
+          environment: {
+            TZ: 'UTC'
+          },
+          volumes: ['caddy_data:/data', 'caddy_config:/config'],
+          command: [
+            'sh',
+            '-c',
+            dedent`
+              cat <<'EOF' > /etc/caddy/${environment}.Caddyfile
+              ${new URL(redirect).hostname.replace('wiki.', '')} {
+                redir ${redirect}{uri} permanent
+              }
+              EOF
+              caddy run --config /etc/caddy/${environment}.Caddyfile --adapter caddyfile
+            `.trim()
+          ]
+        }
+      }),
       'minecraft-main': {
         image: 'itzg/minecraft-server:java24',
         ports: ['25565:25565'],
@@ -21,6 +51,10 @@ export default defineInfra(environment => {
           extra_hosts: ['host.docker.internal:host-gateway']
         })
       }
+    },
+    volumes: {
+      caddy_data: {},
+      caddy_config: {}
     }
   }
 })
