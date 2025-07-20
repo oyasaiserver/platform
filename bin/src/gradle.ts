a809c9942ac04f6596d747bcad6df85155631b0c
Shun Ueda <me@shu.nu>
2025-07-20 12:45:57 -0400

    refactor(bin): sunset bin (#143)

--- bin/src/gradle.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import { directory } from '@oyasaiserver/lib/directory'
-import { $, argv } from 'zx'
-
-await $({
-  nothrow: true,
-  stdio: 'inherit',
-  cwd: directory.root
-})`./gradlew ${argv._}`
