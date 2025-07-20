bdd3f9a9b878fa9bc1a553b76f99d3e30732d77a
Shun Ueda <me@shu.nu>
2025-07-20 11:00:12 -0400

    fix import

--- infra/cdktf/test/integration/stacks/cloudflare-stack.ts
+++ infra/cdktf/test/integration/stacks/cloudflare-stack.ts
@@ -2,7 +2,7 @@ import { describe, test } from 'node:test'
 import { Testing } from 'cdktf'
 import { toBeValidTerraform } from 'cdktf/lib/testing/matchers.js'
 import { ok } from 'node:assert/strict'
-import { CloudflareStack } from '../../src/stacks/cloudflare-stack.ts'
+import { CloudflareStack } from '../../../src/stacks/cloudflare-stack.ts'
 
 await describe(import.meta.filename, async () => {
   await test('Terraform configuration is valid', async () => {
--- infra/cdktf/test/integration/stacks/github-stack.ts
+++ infra/cdktf/test/integration/stacks/github-stack.ts
@@ -2,7 +2,7 @@ import { describe, test } from 'node:test'
 import { Testing } from 'cdktf'
 import { toBeValidTerraform } from 'cdktf/lib/testing/matchers.js'
 import { ok } from 'node:assert/strict'
-import { GitHubStack } from '../../src/stacks/github-stack.ts'
+import { GitHubStack } from '../../../src/stacks/github-stack.ts'
 
 await describe(import.meta.filename, async () => {
   await test('Terraform configuration is valid', async () => {
