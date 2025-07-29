cfc1110c98949ed900156ecb744a8dcfb27a6353
Shun Ueda <me@shu.nu>
2025-07-29 08:07:42 -0400

    impl

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,46 +1,46 @@
 plugins { alias(libs.plugins.spotless) }
 
 buildscript {
-    dependencies {
-        classpath(libs.kotlin.plugin)
-        classpath(libs.shadow.plugin)
-    }
+  dependencies {
+    classpath(libs.kotlin.plugin)
+    classpath(libs.shadow.plugin)
+  }
 
-    repositories { mavenCentral() }
+  repositories { mavenCentral() }
 }
 
 allprojects {
-    repositories {
-        mavenCentral()
-        maven("https://repo.purpurmc.org/snapshots")
-        maven("https://maven.enginehub.org/repo")
-        maven("https://jitpack.io")
-    }
+  repositories {
+    mavenCentral()
+    maven("https://repo.purpurmc.org/snapshots")
+    maven("https://maven.enginehub.org/repo")
+    maven("https://jitpack.io")
+  }
 }
 
 spotless {
-    isEnforceCheck = false
+  isEnforceCheck = false
 
-    kotlin {
-        target("**/*.kt")
-        targetExclude("gen/**/*.kt")
-        ktlint()
-    }
+  kotlin {
+    target("**/*.kt")
+    targetExclude("gen/**/*.kt")
+    ktlint()
+  }
 
-    kotlin {
-        target("**/*.kt")
-        ktfmt().googleStyle()
-    }
+  kotlin {
+    target("**/*.kt")
+    ktfmt().googleStyle()
+  }
 
-    kotlinGradle {
-        target("**/*.gradle.kts")
-        ktfmt().googleStyle()
-        ktlint()
-    }
+  kotlinGradle {
+    target("**/*.gradle.kts")
+    ktfmt().googleStyle()
+    ktlint()
+  }
 
-    java {
-        target("**/*.java")
-        targetExclude("node_modules/**/*")
-        googleJavaFormat()
-    }
+  java {
+    target("**/*.java")
+    targetExclude("node_modules/**/*")
+    googleJavaFormat()
+  }
 }
