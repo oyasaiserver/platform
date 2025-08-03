b7ddb3e0c7773c6a318d88cb30aabbf621a863d2
Shun Ueda <me@shu.nu>
2025-08-02 21:01:26 -0400

    chore(infra): use envs for server config (#156)

--- infra/onprem/src/services/grafana.ts
+++ infra/onprem/src/services/grafana.ts
@@ -1,9 +1,7 @@
 import type { Service } from '@oyasaiserver/json/store/compose_spec'
 
 export const grafana: Service = {
-  depends_on: ['portical'],
   image: 'grafana/grafana',
   ports: ['3000:3000'],
-  volumes: ['grafana_data:/var/lib/grafana'],
-  labels: ['portical.upnp.forward=published']
+  volumes: ['grafana_data:/var/lib/grafana']
 }
--- infra/onprem/src/services/minecraft-main.ts
+++ infra/onprem/src/services/minecraft-main.ts
@@ -2,20 +2,15 @@ import { toolVersions } from '@oyasaiserver/lib/tool-versions'
 import config from '../../config.json' with { type: 'json' }
 import type { Environment } from '@oyasaiserver/schema/environment'
 import type { Service } from '@oyasaiserver/json/store/compose_spec'
+import plugins from '../../plugins.json' with { type: 'json' }
 
 export function createMinecraftMain(environment: Environment): Service {
   return {
-    depends_on: ['mariadb', 'portical'],
+    depends_on: ['mariadb'],
     image: `itzg/minecraft-server:java${toolVersions.java.major}`,
     ports: [
       `${config.services.minecraft.port[environment]}:${config.port.minecraft.value}/${config.port.minecraft.protocol}`,
       `${config.services.minecraftBedrock.port[environment]}:${config.port.minecraftBedrock.value}/${config.port.minecraftBedrock.protocol}`
-    ],
-    labels: [
-      `portical.upnp.forward=${[
-        `${config.services.minecraft.port[environment]}/${config.port.minecraft.protocol}`,
-        `${config.services.minecraftBedrock.port[environment]}/${config.port.minecraftBedrock.protocol}`
-      ]}`
     ],
     restart: 'unless-stopped',
     tty: true,
@@ -28,7 +23,10 @@ export function createMinecraftMain(environment: Environment): Service {
       ENABLE_ROLLING_LOGS: true,
       LOG_TIMESTAMP: true,
       MEMORY: config.services.minecraft.memory[environment],
+      PLUGINS: plugins.map(plugin => plugin.url).join(),
       ICON: 'https://avatars.githubusercontent.com/oyasaiserver'
+      // TODO: turn this on when we have fully managed plugins
+      // REMOVE_OLD_MODS: true,
     },
     volumes: ['./minecraft-main:/data'],
     env_file: '.env'
