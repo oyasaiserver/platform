173ad0df0116a663a1fb014d0b10a391f718719e
Shun Ueda <me@shu.nu>
2025-08-11 23:30:46 -0400

    remove comments

--- infra/onprem/src/plugins.ts
+++ infra/onprem/src/plugins.ts
@@ -3,42 +3,23 @@ const versions = {
 } as const
 
 export const plugins = [
-  // EssentialsX
   `https://github.com/EssentialsX/Essentials/releases/download/${versions.essentials}/EssentialsX-${versions.essentials}.jar`,
-  // EssentialsXChat
   `https://github.com/EssentialsX/Essentials/releases/download/${versions.essentials}/EssentialsXChat-${versions.essentials}.jar`,
-  // EssentialsXSpawn
   `https://github.com/EssentialsX/Essentials/releases/download/${versions.essentials}/EssentialsXSpawn-${versions.essentials}.jar`,
-  // LuckPerms
   'https://download.luckperms.net/1594/bukkit/loader/LuckPerms-Bukkit-5.5.9.jar',
-  // PlugManX
   'https://github.com/Test-Account666/PlugManX/releases/download/2.4.1/PlugManX-2.4.1.jar',
-  // ProtocolLib
   'https://ci.dmulloy2.net/job/ProtocolLib/753/artifact/build/libs/ProtocolLib.jar',
-  // FastAsyncWorldEdit
   'https://ci.athion.net/job/FastAsyncWorldEdit/1160/artifact/artifacts/FastAsyncWorldEdit-Paper-2.13.2-SNAPSHOT-1160.jar',
-  // WorldGuard
   'https://dev.bukkit.org/projects/worldguard/files/6643567/download',
-  // ViaVersion
   'https://hangarcdn.papermc.io/plugins/ViaVersion/ViaVersion/versions/5.4.2/PAPER/ViaVersion-5.4.2.jar',
-  // Floodgate
   'https://download.geysermc.org/v2/projects/floodgate/versions/latest/builds/latest/downloads/spigot',
-  // Geyser
   'https://download.geysermc.org/v2/projects/geyser/versions/latest/builds/latest/downloads/spigot',
-  // DiscordSRV
   'https://github.com/DiscordSRV/DiscordSRV/releases/download/v1.29.0/DiscordSRV-Build-1.29.0.jar',
-  // TAB
   'https://github.com/NEZNAMY/TAB/releases/download/5.2.5/TAB.v5.2.5.jar',
-  // Multiverse-Core
   'https://github.com/Multiverse/Multiverse-Core/releases/download/5.1.2/multiverse-core-5.1.2.jar',
-  // Multiverse-Portals
   'https://github.com/Multiverse/Multiverse-Portals/releases/download/5.1.0/multiverse-portals-5.1.0.jar',
-  // LunaChat
   'https://github.com/f1w3/LunaChat/releases/download/v3.0.16-fix/LunaChat.jar',
-  // DisplayShops
   'https://github.com/XZot1K/DisplayShops/releases/download/2.0/DisplayShops_2.0.1.jar',
-  // Terra
   'https://cdn.modrinth.com/data/FIlZB9L0/versions/GXo6GGY8/Terra-bukkit-6.6.3-BETA%2Bcc5258ce7-shaded.jar',
-  // VoidGen
   'https://github.com/NicoNekoDev/VoidGen/releases/download/2.2.9/VoidGen-2.2.9.jar'
 ] as const
