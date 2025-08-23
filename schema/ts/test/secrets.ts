1e0ecddac3407f401e5f5f13051fe85acadf5a2f
Shun Ueda <me@shu.nu>
2025-08-23 12:53:33 -0400

    refactor: rename envs to secrets (#176)

--- schema/ts/test/secrets.ts
+++ schema/ts/test/secrets.ts
@@ -1,18 +1,17 @@
-import { readdir } from 'node:fs/promises'
+import { readdir, readFile } from 'node:fs/promises'
 import { join } from 'node:path'
 import { suite, test } from 'node:test'
 import { parseEnv } from 'node:util'
 import { directory } from '@oyasaiserver/lib/directory'
-import { readFileContent } from '@oyasaiserver/lib/fs'
 import { secrets } from '../src/secrets.ts'
 
 await suite(import.meta.filename, async () => {
   await test('secrets schema should match .env', async () => {
-    const dir = `${directory.root}/envs`
+    const dir = `${directory.root}/secrets`
     const environments = await readdir(dir)
     for (const environment of environments) {
       const path = join(dir, environment, '.env')
-      await readFileContent(path).then(parseEnv).then(secrets.parse)
+      await readFile(path, 'utf-8').then(parseEnv).then(secrets.parse)
     }
   })
 })
