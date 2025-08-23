00894027532396eab97845f6b176915fa7f0d9e9
Shun Ueda <me@shu.nu>
2025-08-22 20:23:23 -0400

    refactor: remove codegen (#173)

--- infra/onprem/src/index.ts
+++ infra/onprem/src/index.ts
@@ -1,29 +1,26 @@
 import { mariadb } from './services/mariadb.ts'
-import { createMinecraftMain } from './services/minecraft-main.ts'
-import type { ComposeSpec } from '@oyasaiserver/json/store/compose_spec'
-import type { Environment } from '@oyasaiserver/schema/environment'
 import { prometheus } from './services/prometheus.ts'
 import { grafana } from './services/grafana.ts'
 import { nodeExporter } from './services/node-exporter.ts'
+import { secrets } from "@oyasaiserver/lib/secrets";
+import { minecraftMain } from "./services/minecraft-main.ts";
 
-export function createOnpremInfra(environment: Environment): ComposeSpec {
-  return {
-    services: {
-      mariadb,
-      'minecraft-main': createMinecraftMain(environment),
+export const onpremInfra = {
+  services: {
+    mariadb,
+    'minecraft-main': minecraftMain,
 
-      ...(environment === 'production' && {
-        prometheus,
-        grafana,
-        'node-exporter': nodeExporter
-      })
-    },
+    ...(secrets.ENVIRONMENT === 'production' && {
+      prometheus,
+      grafana,
+      'node-exporter': nodeExporter
+    })
+  },
 
-    volumes: {
-      ...(environment === 'production' && {
-        prometheus_data: {},
-        grafana_data: {}
-      })
-    }
+  volumes: {
+    ...(secrets.ENVIRONMENT === 'production' && {
+      prometheus_data: {},
+      grafana_data: {}
+    })
   }
-}
+} as const
