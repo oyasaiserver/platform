2ab343dbca52f1e084758b710dd3c501b7f6114a
Shun Ueda <me@shu.nu>
2025-08-28 08:27:04 -0400

    feat(infra): minecraft-backup-and-restore (#209)

--- infra/onprem/src/index.ts
+++ infra/onprem/src/index.ts
@@ -4,7 +4,12 @@ import { grafana } from './services/grafana.ts'
 import { nodeExporter } from './services/node-exporter.ts'
 import { secrets } from '@oyasaiserver/lib/secrets'
 import { minecraftMain } from './services/minecraft-main.ts'
+import {
+  minecraftMainBackup,
+  minecraftMainBackupRestore
+} from './services/minecraft-main-backup.ts'
 import type { Compose } from '@json-types/compose'
+import { mariadbBackup } from './services/mariadb-backup.ts'
 
 export const onpremInfra: Compose = {
   services: {
@@ -12,9 +17,17 @@ export const onpremInfra: Compose = {
     'minecraft-main': minecraftMain,
 
     ...(secrets.ENVIRONMENT === 'production' && {
+      'minecraft-main-backup': minecraftMainBackup,
+      'mariadb-backup': mariadbBackup,
+
       prometheus,
       grafana,
       'node-exporter': nodeExporter
+    }),
+
+    ...(secrets.ENVIRONMENT === 'development' && {
+      'minecraft-main-backup-restore': minecraftMainBackupRestore
+      // TODO 'mariadb-backup-restore': mariadbBackupRestore
     })
   },
 
--- infra/onprem/src/services/minecraft-main-backup.ts
+++ infra/onprem/src/services/minecraft-main-backup.ts
@@ -1,22 +1,36 @@
 import type { Service } from '@json-types/compose'
 import { secrets } from '@oyasaiserver/lib/secrets'
 
+const base = {
+  BACKUP_METHOD: 'restic',
+  RESTIC_PASSWORD: secrets.RESTIC_PASSWORD,
+  AWS_ACCESS_KEY_ID: secrets.CLOUDFLARE_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY: secrets.CLOUDFLARE_SECRET_ACCESS_KEY,
+  RESTIC_VERBOSE: true,
+  // bucket is production by default
+  RESTIC_REPOSITORY: `s3:https://${secrets.CLOUDFLARE_ACCOUNT_ID}.r2.cloudflarestorage.com/production/minecraft-main-backup`
+}
+
 export const minecraftMainBackup: Service = {
   depends_on: ['minecraft-main'],
   image: 'itzg/mc-backup',
   restart: 'unless-stopped',
   environment: {
+    ...base,
     RCON_HOST: 'minecraft-main',
     RCON_PASSWORD: secrets.RCON_PASSWORD,
-    BACKUP_METHOD: 'restic',
-    RESTIC_PASSWORD: secrets.RESTIC_PASSWORD,
-    RESTIC_REPOSITORY: `s3:https://${secrets.CLOUDFLARE_ACCOUNT_ID}.r2.cloudflarestorage.com/${secrets.R2_BUCKET_NAME}/minecraft-main-backup`,
-    AWS_ACCESS_KEY_ID: secrets.CLOUDFLARE_ACCESS_KEY_ID,
-    AWS_SECRET_ACCESS_KEY: secrets.CLOUDFLARE_SECRET_ACCESS_KEY,
     EXCLUDES: '*.jar,cache,logs,*.tmp,bluemap',
     BACKUP_INTERVAL: '6h',
-    PRUNE_RESTIC_RETENTION: '--keep-daily 7 --keep-weekly 4 --keep-monthly 3',
-    RESTIC_VERBOSE: true
+    PRUNE_RESTIC_RETENTION: '--keep-daily 7 --keep-weekly 4 --keep-monthly 3'
   },
   volumes: ['./minecraft-main:/data:ro']
+}
+
+export const minecraftMainBackupRestore: Service = {
+  image: 'itzg/mc-backup',
+  restart: 'no',
+  entrypoint: '',
+  command: 'restic restore latest --target /data',
+  environment: base,
+  volumes: ['./minecraft-main:/data']
 }
