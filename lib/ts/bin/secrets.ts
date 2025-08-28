755b3881b6c9af706a830110a4cf62ab8021ab97
Shun Ueda <me@shu.nu>
2025-08-27 23:36:56 -0400

    impl

--- lib/ts/bin/secrets.ts
+++ lib/ts/bin/secrets.ts
@@ -3,11 +3,30 @@ import { secrets } from '../src/secrets.ts'
 import { $, argv } from 'zx'
 import { env } from 'node:process'
 
-await $({
-  env: {
-    ...env,
-    ...secrets
-  },
-  nothrow: true,
-  stdio: 'inherit'
-})`${argv._}`
+switch (argv._[0]) {
+  case 'run': {
+    await $({
+      env: {
+        ...env,
+        ...secrets
+      },
+      nothrow: true,
+      stdio: 'inherit'
+    })`${argv._.splice(1)}`
+    break
+  }
+  case 'set': {
+    break
+  }
+  case 'get': {
+    break
+  }
+  case undefined: {
+    console.error('No command provided.')
+    break
+  }
+  default: {
+    console.error('Unknown command:', argv[0])
+    break
+  }
+}
