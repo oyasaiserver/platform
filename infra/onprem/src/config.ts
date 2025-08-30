ea01a61a613e5b98ae294fc70b0937b985a0cc77
Shun Ueda <me@shu.nu>
2025-08-30 14:34:44 -0400

    refactor(infra): remove port config (#216)

--- infra/onprem/src/config.ts
+++ infra/onprem/src/config.ts
@@ -9,27 +9,5 @@ export const config = {
         local: '5G'
       }
     }
-  },
-  port: {
-    ssh: {
-      protocol: 'tcp',
-      value: 22
-    },
-    http: {
-      protocol: 'tcp',
-      value: 80
-    },
-    https: {
-      protocol: 'tcp',
-      value: 443
-    },
-    minecraft: {
-      protocol: 'tcp',
-      value: 25565
-    },
-    minecraftBedrock: {
-      protocol: 'udp',
-      value: 19132
-    }
   }
 } as const
