b9449649f35eeb891976411b7fced89b0c57ab74
Shun Ueda <me@shu.nu>
2025-09-06 14:43:40 -0400

    impl

--- secrets/src/secrets.ts
+++ secrets/src/secrets.ts
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 import { secrets } from '@oyasaiserver/secrets'
-import { env } from 'node:process'
-import { $, argv } from 'zx'
+import { spawn } from 'node:child_process'
+import { argv, env } from 'node:process'
 
-await $({
+const [command, ...args] = argv.splice(3)
+
+spawn(command!, args, {
+  stdio: 'inherit',
   env: {
     ...env,
     ...secrets
-  },
-  nothrow: true,
-  stdio: 'inherit'
-})`${argv._}`
+  }
+})
