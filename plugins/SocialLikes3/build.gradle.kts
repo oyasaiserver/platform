edae28beb56616a38677daee7d51cc3a1eb0d058
Shun Ueda <me@shu.nu>
2025-09-14 23:18:18 -0400

    refactor(sl): remove ua api local deps (#243)

--- plugins/SocialLikes3/build.gradle.kts
+++ plugins/SocialLikes3/build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
   compileOnly(group = "com.github.Realizedd", name = "TokenManager", version = "3.2.4") {
     isTransitive = false
   }
-  implementation(files("libs/ultimateadvancementapi-shadeable-2.6.0.jar"))
+  implementation("com.frengor:ultimateadvancementapi:2.6.0")
 }
 
 tasks.apply {
--- secrets/src/secrets.ts
+++ secrets/src/secrets.ts
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 import { secrets } from '@oyasaiserver/secrets'
+import { ok } from 'node:assert'
 import { spawn } from 'node:child_process'
 import { argv, env } from 'node:process'
 
 const [command, ...args] = argv.splice(3)
 
-spawn(command!, args, {
+ok(command)
+
+spawn(command, args, {
   stdio: 'inherit',
   env: {
     ...env,
