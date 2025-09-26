0640e22f5f81c6f8e706bbee78d84a2c88345303
Shun Ueda <me@shu.nu>
2025-09-26 09:34:04 -0400

    feat(secrets): local secret overrides (#266)

--- secrets/src/index.ts
+++ secrets/src/index.ts
@@ -6,10 +6,18 @@ import { schema } from './schema.ts'
 
 const envfile = `${import.meta.dirname}/../${env.ENVIRONMENT ?? 'local'}/.env`
 
-export const secrets = schema.parse(
+export const secretsNoOverrides = schema.parse(
   parse(await readFile(envfile), {
     privateKey:
       env.DOTENV_PRIVATE_KEY ||
       (await readFile(`${envfile}.keys`, 'utf-8').then(it => parseEnv(it).DOTENV_PRIVATE_KEY))
   })
+)
+
+export const secrets = schema.parse(
+  Object.fromEntries(
+    Object.entries(secretsNoOverrides).map(([key, value]) => {
+      return [key, env[key] ?? value]
+    })
+  )
 )
