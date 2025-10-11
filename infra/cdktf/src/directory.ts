f1ddbf426cbad84984865e4c283be74709108c0f
Shun Ueda <me@shu.nu>
2025-10-11 14:50:30 -0400

    feat: non-ignore hashdir

--- infra/cdktf/src/directory.ts
+++ infra/cdktf/src/directory.ts
@@ -1,30 +1,42 @@
-import ignore from 'ignore'
+import ignore, { type Ignore } from 'ignore'
 import { createHash } from 'node:crypto'
-import { readdirSync, readFileSync } from 'node:fs'
+import { type PathLike, readdirSync, readFileSync } from 'node:fs'
 import { join, relative } from 'node:path'
 
 export const directory = {
-  root: join(import.meta.dirname, '../../..'),
-  hashSync(...directories: readonly string[]) {
-    const ignoreFile = readFileSync(join(directory.root, '.gitignore'), 'utf-8')
-    const gitignore = ignore().add(ignoreFile)
+  root: join(import.meta.dirname, '../../..')
+} as const
 
-    const hash = createHash('sha256')
+function createIgnore(): Ignore {
+  const path = join(directory.root, '.gitignore')
+  const ignoreFile = readFileSync(path, 'utf-8')
+  return ignore().add(ignoreFile)
+}
 
-    for (const dir of directories.toSorted()) {
-      const files = readdirSync(dir, {
-        withFileTypes: true,
-        recursive: true
-      })
-      for (const file of files.toSorted()) {
-        const path = join(file.parentPath, file.name)
-        const relativePath = relative(directory.root, path)
-        if (file.isFile() && !gitignore.ignores(relativePath)) {
-          hash.update(relativePath).update(readFileSync(path))
-        }
+export function hashdirSync(
+  directories: readonly PathLike[],
+  options?: {
+    noIgnore?: boolean
+  }
+): string {
+  const ignore = options?.noIgnore ? undefined : createIgnore()
+  const hash = createHash('sha256')
+  for (const dir of directories.toSorted()) {
+    const files = readdirSync(dir, {
+      withFileTypes: true,
+      recursive: true
+    })
+    for (const file of files.toSorted()) {
+      const path = join(file.parentPath, file.name)
+      const relativePath = relative(directory.root, path)
+      if (!file.isFile()) {
+        continue
+      }
+      if (!ignore || !ignore.ignores(relativePath)) {
+        const content = readFileSync(path)
+        hash.update(relativePath).update(content)
       }
     }
-
-    return hash.digest('hex')
   }
-} as const
+  return hash.digest('hex')
+}
