8cb02b5cbeacd4399a9d279d1d23a0018a51d6ce
takucan <takucancrafter@gmail.com>
2025-10-14 15:51:42 +0900

    connect dp

--- plugins/OyasaiAdminTools/build.gradle.kts
+++ plugins/OyasaiAdminTools/build.gradle.kts
@@ -1,17 +1,13 @@
 plugins {
   kotlin("jvm")
   id("com.gradleup.shadow")
-  `java-library`
 }
 
 dependencies {
-  compileOnly(libs.placeholderapi)
   compileOnly(libs.purpur.api)
-  compileOnly(libs.nuvotifier)
-  compileOnly(libs.vault.api)
   compileOnly(libs.luckperms.api)
-  compileOnly(libs.tokenmanager) { isTransitive = false }
   compileOnly(project(":plugins:SocialLikes3"))
+  compileOnly(project(":plugins:DynamicProfile"))
   implementation(libs.kotlin.stdlib)
   implementation(libs.gson)
 }
