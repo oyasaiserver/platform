4287b85690ea6418b941510e1a9cb7fbe67c2c12
Shun Ueda <me@shu.nu>
2025-10-13 21:00:50 -0400

    refactor

--- infra/cdktf/src/fs.ts
+++ infra/cdktf/src/fs.ts
@@ -1,6 +1,11 @@
 import { hashPath as _hashPath } from 'cdktf/lib/private/fs.js'
 import { createHash } from 'node:crypto'
 import type { PathLike } from 'node:fs'
+import { join } from 'node:path'
+
+export const directory = {
+  root: join(import.meta.dirname, '../../..')
+} as const
 
 export function hashPath(path: PathLike): string {
   return _hashPath(path.toString())
