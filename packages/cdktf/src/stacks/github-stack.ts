5b8c6990da294379e57568f0caf003f62d6f1e52
Shun Ueda <me@shu.nu>
2025-10-20 23:40:01 -0400

    impl

---
+++ packages/cdktf/src/stacks/github-stack.ts
@@ -0,0 +1,23 @@
+import { DataGithubRepository } from '@cdktf/provider-github/lib/data-github-repository/index.js'
+import { GithubProvider } from '@cdktf/provider-github/lib/provider/index.js'
+import type { Construct } from 'constructs'
+import { OyasaiTerraformStack } from './oyasai-terraform-stack.ts'
+
+export class GitHubStack extends OyasaiTerraformStack {
+  public constructor(scope: Construct, id: string) {
+    super(scope, id)
+
+    new GithubProvider(this, id, {
+      owner: 'oyasaiserver',
+      appAuth: {
+        id: this.secrets.GITHUB_APP_ID,
+        installationId: this.secrets.GITHUB_APP_INSTALLATION_ID,
+        pemFile: this.secrets.GITHUB_APP_PEM_FILE
+      }
+    })
+
+    new DataGithubRepository(this, 'repository', {
+      name: 'platform'
+    })
+  }
+}
