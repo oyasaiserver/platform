cc51c56bb35954bfafd5a2727aa8e2e3ecb5822f
Shun Ueda <me@shu.nu>
2025-10-21 10:19:09 -0400

    fix names

--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -1,10 +1,11 @@
+import { cloudflare } from '@cloudflare/vite-plugin'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), cloudflare()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
