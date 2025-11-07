a1e1023b677e5a5db2c92775a54d7592d17e5bc6
Shun Ueda <me@shu.nu>
2025-11-06 22:01:25 -0500

    fix: name

--- packages/minecraft-server/test/plugins.ts
+++ packages/minecraft-server/test/plugins.ts
@@ -4,12 +4,12 @@ import { downloadPlugins } from '../src/download.ts'
 import { pluginRegistry, type RegistryId } from '../src/plugin-registry.ts'
 
 await suite(import.meta.filename, async () => {
-  const ids = (Object.keys(pluginRegistry) as RegistryId[]).filter(id => {
+  const remoteIds = (Object.keys(pluginRegistry) as RegistryId[]).filter(id => {
     return pluginRegistry[id].type !== 'local'
   })
 
   await test('all plugins can be downloaded', async () => {
     await using tempdir = await mkdtempDisposable('test-plugins-')
-    await downloadPlugins(tempdir.path, ids)
+    await downloadPlugins(tempdir.path, remoteIds)
   })
 })
