730698e585a82897dc6addf823dd5c45acd58c9f
Shun Ueda <me@shu.nu>
2025-11-06 22:01:14 -0500

    fix: only remote

--- packages/minecraft-server/test/plugins.ts
+++ packages/minecraft-server/test/plugins.ts
@@ -4,7 +4,9 @@ import { downloadPlugins } from '../src/download.ts'
 import { pluginRegistry, type RegistryId } from '../src/plugin-registry.ts'
 
 await suite(import.meta.filename, async () => {
-  const ids = Object.keys(pluginRegistry) as RegistryId[]
+  const ids = (Object.keys(pluginRegistry) as RegistryId[]).filter(id => {
+    return pluginRegistry[id].type !== 'local'
+  })
 
   await test('all plugins can be downloaded', async () => {
     await using tempdir = await mkdtempDisposable('test-plugins-')
