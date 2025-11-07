fcb1d398a8c1b0d59d92ab4416b72e9ff95bb097
Shun Ueda <me@shu.nu>
2025-11-06 23:57:03 -0500

    feat: schematics service

--- plugins/Vertex/src/main/kotlin/io/oyasai/vertex/services/Service.kt
+++ plugins/Vertex/src/main/kotlin/io/oyasai/vertex/services/Service.kt
@@ -1,8 +1,11 @@
 package io.oyasai.vertex.services
 
+import org.bukkit.command.Command
 import org.bukkit.event.Listener
 
-interface Service : Listener {
+abstract class Service : Listener {
+  open val commands: List<Command> = listOf()
+
   fun onLoad() {}
 
   fun onEnable() {}
--- plugins/Vertex/src/main/kotlin/io/oyasai/vertex/services/sociallikes/SocialLikesService.kt
+++
@@ -1,7 +0,0 @@
-package io.oyasai.vertex.services.sociallikes
-
-import io.oyasai.vertex.services.Service
-
-object SocialLikesService : Service {
-  override fun onEnable() {}
-}
