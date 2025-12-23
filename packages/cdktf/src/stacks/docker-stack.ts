376f4cc5ae3b7832e30ed59df8dcc8be16450343
Shun Ueda <me@shu.nu>
2025-12-23 17:11:37 -0500

    refactor: automatic motd

--- packages/cdktf/src/stacks/docker-stack.ts
+++ packages/cdktf/src/stacks/docker-stack.ts
@@ -99,6 +99,7 @@ export class DockerStack extends OyasaiTerraformStack {
           USE_MEOWICE_FLAGS: this.environment !== 'local',
           ENABLE_ROLLING_LOGS: true,
           LOG_TIMESTAMP: true,
+          MOTD: `§l§r                 §b§lOyasai§f§lServer§7 [v${DockerStack.minecraftVersion}]§r\n§l§f            建築勢は集合だ！建築！建築！建築！！！`,
           MEMORY: this.envAwareConfig({
             production: '32G',
             development: '12G',
