e1064dc543531610b5a6b1c212cf9c59b9ccb998
Shun Ueda <me@shu.nu>
2025-12-26 17:08:46 -0500

    refactor: modularize plugins

--- packages/minecraft-server/scripts/download-plugins.ts
+++ packages/minecraft-server/scripts/download-plugins.ts
@@ -1,7 +1,86 @@
-import { plugins } from '@oyasaiserver/plugins'
 import { downloadPlugins } from '@oyasaiserver/plugins/download'
 import { readEnvironment } from '@oyasaiserver/secrets/environment'
 import { glob, rm } from 'node:fs/promises'
+
+import type { Environment } from '@oyasaiserver/secrets/environment'
+import { type RegistryId } from '@oyasaiserver/plugins/registry'
+
+const local: RegistryId[] = [
+  'essentialsx',
+  'fastasyncworldedit',
+  'luckperms',
+  'plugmanx',
+  'protocollib',
+  'vault',
+  'nuvotifier',
+  'vertex'
+]
+
+const development: RegistryId[] = local.concat(
+  'bkcommonlib',
+  'coreprotect',
+  'decentholograms',
+  'essentialsxchat',
+  'essentialsxspawn',
+  'fastasyncvoxelsniper',
+  'multiversecore',
+  'multiverseprotals',
+  'placeholderapi',
+  'tab',
+  'terra',
+  'veinminer',
+  'viaversion',
+  'worldguard',
+  'venturechat',
+  'particlehats',
+  'skinsrestorer',
+  'protocollib',
+  'advancedban',
+  'signshop',
+  'mypet',
+  'minepacks',
+  'slotmachine',
+  'mycommand',
+  'simpleelevator',
+  'inventoryshop',
+  'crackshotguns',
+  'joincommands',
+  'tntruneloaded',
+  'gsit',
+  'lwc',
+  'skript',
+  'zvoteparty',
+  'floodgate',
+  'geyser',
+  'openinv',
+  'lunachat',
+  'worldeditsui',
+  'worldborder',
+  'imageonmap',
+  'chestcommands',
+  'arceon',
+  'ezedits',
+  'oyasaivehicles',
+  'tokenmanager',
+  'dynamicprofile',
+  'oyasaiadmintools',
+  'oyasaiutilities',
+  'painttools',
+  'sociallikes3',
+  'socialvotes',
+  'tpswitch',
+  'vertex',
+  'pvparena',
+  'oyasaipets'
+)
+
+const production: RegistryId[] = development.concat('discordsrv', 'bluemap')
+
+const plugins: Record<Environment, readonly RegistryId[]> = {
+  local,
+  development,
+  production
+}
 
 if (import.meta.main) {
   const environment = readEnvironment()
