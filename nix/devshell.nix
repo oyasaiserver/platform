1b303b35437db7a5ac435d217fe79241a86824e5
Shun Ueda <me@shu.nu>
2025-12-26 14:55:15 -0500

    refactor: cleanup devshell

--- nix/devshell.nix
+++ nix/devshell.nix
@@ -1,10 +1,9 @@
 { flake-parts-lib, ... }:
 {
   options.perSystem = flake-parts-lib.mkPerSystemOption (
-    { pkgs, config, ... }:
+    { pkgs, ... }:
     {
       devShells.default = pkgs.mkShell {
-        name = "anterior-npm-devshell";
         packages = with pkgs; [
           nodejs_24
           javaPackages.compiler.temurin-bin.jdk-25
