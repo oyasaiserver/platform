7457813917d02e385de68b0907836a7e5e159a3b
Shun Ueda <me@shu.nu>
2026-01-04 17:23:03 -0500

    revert: restore original

---
+++ packages/plugin-registry/src/download.ts
@@ -0,0 +1,75 @@
+import { ModrinthV2Client } from "@xmcl/modrinth";
+import { ok } from "node:assert/strict";
+import type { PathLike } from "node:fs";
+import { readFile, writeFile } from "node:fs/promises";
+import { join } from "node:path";
+import { URL } from "node:url";
+import { type PluginDefinition, registry, type RegistryId } from "./index.ts";
+
+async function toDownloadUrl(
+  definition: PluginDefinition,
+  version: string,
+): Promise<URL> {
+  switch (definition.type) {
+    case "url":
+      return new URL(definition.url);
+    case "spiget": {
+      return new URL(
+        `https://api.spiget.org/v2/resources/${definition.id}/download`,
+      );
+    }
+    case "modrinth": {
+      const client = new ModrinthV2Client();
+      const project = await client.getProject(definition.slug);
+      const projectVersions = await client.getProjectVersions(project.id, {
+        gameVersions: [version],
+        loaders: ["paper", "spigot", "bukkit"],
+      });
+      const url = projectVersions
+        .flatMap((version) => version.files)
+        .map((file) => file.url)
+        .at(0);
+      ok(url, `No download URL found for modrinth plugin: ${definition.slug}`);
+      return new URL(url);
+    }
+    case "github": {
+      return new URL(
+        `https://github.com/${definition.owner}/${definition.repo}/releases/download/${definition.tag}/${definition.name}`,
+      );
+    }
+    case "local": {
+      return new URL(`file://${definition.path}`);
+    }
+  }
+}
+
+export async function downloadJar(
+  definition: PluginDefinition,
+  version: string,
+): Promise<Uint8Array> {
+  const url = await toDownloadUrl(definition, version);
+  if (url.protocol === "file:") {
+    return readFile(url.pathname); // fetching `file:` protocol is not supported
+  }
+  const response = await fetch(url);
+  const jarHeaders = [
+    "application/zip",
+    "application/java-archive",
+    "application/octet-stream",
+  ];
+  ok(jarHeaders.includes(response.headers.get("Content-Type") ?? ""));
+  const arrayBuffer = await response.arrayBuffer();
+  return new Uint8Array(arrayBuffer);
+}
+
+export async function downloadPlugins(
+  dir: PathLike,
+  ids: readonly RegistryId[],
+  version: string,
+): Promise<void> {
+  for (const id of ids) {
+    const bytes = await downloadJar(registry[id], version);
+    const path = join(dir.toString(), `${id}.jar`);
+    await writeFile(path, bytes);
+  }
+}
