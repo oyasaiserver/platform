f30a110f57bedf82d46f129ffe0ee0d05e6879e7
sahyuya <nananana0852426@gmail.com>
2026-02-15 22:34:46 +0900

    add src

---
+++ plugins/EntityPose/src/main/kotlin/me/ankokunsan/entityPose/CustomHead.kt
@@ -0,0 +1,33 @@
+package me.ankokunsan.entityPose
+
+import org.bukkit.Bukkit
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.SkullMeta
+import java.net.URL
+import java.util.*
+
+object CustomHead { // objectにすると便利
+    @Suppress("DEPRECATION")
+    fun get(textureId: String): ItemStack {
+        val head = ItemStack(Material.PLAYER_HEAD)
+        val meta = head.itemMeta as? SkullMeta ?: return head
+
+        // IDだけ渡された場合にフルURLに補完する
+        val urlString = if (textureId.startsWith("http")) textureId
+        else "http://textures.minecraft.net/texture/$textureId"
+
+        val profile = Bukkit.createPlayerProfile(UUID.randomUUID())
+        val textures = profile.textures
+        try {
+            textures.skin = URL(urlString)
+            profile.setTextures(textures)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        meta.ownerProfile = profile
+        head.itemMeta = meta
+        return head
+    }
+}
