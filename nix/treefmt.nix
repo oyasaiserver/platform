f7a817ec201c76b4d16e94806ef18c9a8225a447
Shun Ueda <me@shu.nu>
2026-02-18 00:44:10 -0500

    chore: update

--- nix/treefmt.nix
+++ nix/treefmt.nix
@@ -3,67 +3,53 @@
   options.perSystem = flake-parts-lib.mkPerSystemOption (
     { ... }:
     {
-      treefmt = {
-        programs = {
-          # keep-sorted start block=yes
-          autocorrect = {
-            enable = true;
-            excludes = [ "plugins/*" ];
-          };
-          jsonfmt = {
-            enable = true;
-          };
-          keep-sorted = {
-            enable = true;
-          };
-          ktfmt = {
-            enable = true;
-          };
-          mdformat = {
-            enable = true;
-            settings = {
-              wrap = 80;
-            };
-          };
-          mdsh = {
-            enable = true;
-          };
-          nixfmt = {
-            enable = true;
-            strict = true;
-          };
-          prettier = {
-            enable = true;
-          };
-          shellcheck = {
-            enable = true;
-          };
-          shfmt = {
-            enable = true;
-            simplify = false;
-          };
-          toml-sort = {
-            enable = true;
-          };
-          typos = {
-            enable = true;
-            excludes = [ "plugins/*" ];
-          };
-          yamlfmt = {
-            enable = true;
-            settings.formatter = {
-              type = "basic";
-              # allow single empty line
-              retain_line_breaks_single = true;
-              # https://github.com/google/yamlfmt/issues/84
-              scan_folded_as_literal = true;
-              # according to the doc - "crlf on Windows, lf otherwise". Explicitly setting
-              # to avoid inconsistency.
-              line_ending = "lf";
-            };
-          };
-          # keep-sorted end
+      treefmt.programs = {
+        # keep-sorted start block=yes
+        actionlint.enable = true;
+        autocorrect = {
+          enable = true;
+          excludes = [ "plugins/*" ];
         };
+        jsonfmt.enable = true;
+        keep-sorted.enable = true;
+        ktfmt = {
+          enable = true;
+        };
+        mdformat = {
+          enable = true;
+          settings = {
+            wrap = 80;
+          };
+        };
+        nixfmt = {
+          enable = true;
+          strict = true;
+        };
+        prettier.enable = true;
+        shellcheck.enable = true;
+        shfmt = {
+          enable = true;
+          simplify = false;
+        };
+        toml-sort.enable = true;
+        typos = {
+          enable = true;
+          excludes = [ "plugins/*" ];
+        };
+        yamlfmt = {
+          enable = true;
+          settings.formatter = {
+            type = "basic";
+            # allow single empty line
+            retain_line_breaks_single = true;
+            # https://github.com/google/yamlfmt/issues/84
+            scan_folded_as_literal = true;
+            # according to the doc - "crlf on Windows, lf otherwise". Explicitly setting
+            # to avoid inconsistency.
+            line_ending = "lf";
+          };
+        };
+        # keep-sorted end
       };
     }
   );
