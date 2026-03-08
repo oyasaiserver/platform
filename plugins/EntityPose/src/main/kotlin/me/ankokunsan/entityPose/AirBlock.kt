92479c17e7c7498d5c10f40d45b7acbf856e6bf7
ankokun <ankokun1010@gmail.com>
2026-03-09 00:07:21 +0900

    format

--- plugins/EntityPose/src/main/kotlin/me/ankokunsan/entityPose/AirBlock.kt
+++ plugins/EntityPose/src/main/kotlin/me/ankokunsan/entityPose/AirBlock.kt
@@ -7,70 +7,75 @@ import org.bukkit.entity.Player
 import org.bukkit.scheduler.BukkitRunnable
 
 object AirBlock {
-    private val glowingTarget = mutableMapOf<java.util.UUID, Entity>()
+  private val glowingTarget = mutableMapOf<java.util.UUID, Entity>()
 
-    fun airblockplace(player: Player) {
-        val block = player.location.subtract(0.0,1.0,0.0).block
+  fun airblockplace(player: Player) {
+    val block = player.location.subtract(0.0, 1.0, 0.0).block
 
-        if (block.type != Material.AIR) return
+    if (block.type != Material.AIR) return
 
-        val nearby = block.world.getNearbyEntities(block.location.add(0.5, 0.5, 0.5), 0.5, 0.5, 0.5)
-        if (nearby.any { it != player }) return
+    val nearby = block.world.getNearbyEntities(block.location.add(0.5, 0.5, 0.5), 0.5, 0.5, 0.5)
+    if (nearby.any { it != player }) return
 
-        // 足場を設置
-        block.setType(Material.GLASS, false)
-        val centerLoc = block.location.add(0.5, 0.0, 0.5)
-        centerLoc.world?.playSound(
-            centerLoc, Sound.ENTITY_CHICKEN_EGG, 1.0f, 2.0f)
+    // 足場を設置
+    block.setType(Material.GLASS, false)
+    val centerLoc = block.location.add(0.5, 0.0, 0.5)
+    centerLoc.world?.playSound(centerLoc, Sound.ENTITY_CHICKEN_EGG, 1.0f, 2.0f)
 
-        object : BukkitRunnable() {
-            override fun run() {
-                // 1. プレイヤーとブロックの水平距離を計算
-                // (ブロックの中心点とプレイヤーの現在地の平面距離)
-                val pLoc = player.location
-                val distance = pLoc.distance(centerLoc.clone().apply { y = pLoc.y })
-                // 2. 判定：1.1マス（ブロックの端）より外に出たら消去してタスク終了
-                if (distance >= 1.1) {
-                    removeBlock()
-                    player.sendMessage("§6[EntityPose] §c足場から離れたため消去しました")
-                    centerLoc.world?.playSound(centerLoc,Sound.BLOCK_GLASS_BREAK,2.0f,1.0f)
-                    this.cancel()
-                    return
-                }
+    object : BukkitRunnable() {
+          override fun run() {
+            // 1. プレイヤーとブロックの水平距離を計算
+            // (ブロックの中心点とプレイヤーの現在地の平面距離)
+            val pLoc = player.location
+            val distance = pLoc.distance(centerLoc.clone().apply { y = pLoc.y })
+            // 2. 判定：1.1マス（ブロックの端）より外に出たら消去してタスク終了
+            if (distance >= 1.1) {
+              removeBlock()
+              player.sendMessage("§6[EntityPose] §c足場から離れたため消去しました")
+              centerLoc.world?.playSound(centerLoc, Sound.BLOCK_GLASS_BREAK, 2.0f, 1.0f)
+              this.cancel()
+              return
             }
+          }
 
-            private fun removeBlock() {
-                if (block.type == Material.GLASS) {
-                    block.type = Material.AIR
-                    // サバイバルでの表示バグ（透明な壁が残る現象）を防止
-                    player.sendBlockChange(block.location, Material.AIR.createBlockData())
-                }
-                cancel()
+          private fun removeBlock() {
+            if (block.type == Material.GLASS) {
+              block.type = Material.AIR
+              // サバイバルでの表示バグ（透明な壁が残る現象）を防止
+              player.sendBlockChange(block.location, Material.AIR.createBlockData())
             }
-        }.runTaskTimer(EntityPose.INSTANCE, 0L, 20L) // 0Lにすることで、設置直後から判定を開始
-    }
+            cancel()
+          }
+        }
+        .runTaskTimer(EntityPose.INSTANCE, 0L, 20L) // 0Lにすることで、設置直後から判定を開始
+  }
 
-    fun startglowing(player: Player) {
-        object : BukkitRunnable() {
-            override fun run() {
-                val hand = player.inventory.itemInMainHand
+  fun startglowing(player: Player) {
+    object : BukkitRunnable() {
+          override fun run() {
+            val hand = player.inventory.itemInMainHand
 
-                if (!isEntiStick(hand)||  !player.isOnline) {
-                glowingTarget[player.uniqueId]?.isGlowing = false
-                    this.cancel()
-                    return
-                }
-                val result = player.world.rayTraceEntities(player.eyeLocation, player.location.direction, 7.0, 0.5) { it != player }
-                val target = result?.hitEntity ?: return
-
-                val lastTarget = glowingTarget[player.uniqueId]
-                if (target != lastTarget) {
-                    lastTarget?.isGlowing = false
-
-                    target.isGlowing = true
-                    glowingTarget[player.uniqueId] = target
-                }
+            if (!isEntiStick(hand) || !player.isOnline) {
+              glowingTarget[player.uniqueId]?.isGlowing = false
+              this.cancel()
+              return
             }
-        }.runTaskTimer(EntityPose.INSTANCE, 0L, 2L) // 0Lにすることで、設置直後から判定を開始
-    }
+            val result =
+                player.world.rayTraceEntities(
+                    player.eyeLocation, player.location.direction, 7.0, 0.5) {
+                      it != player
+                    }
+            val target = result?.hitEntity ?: return
+
+            val lastTarget = glowingTarget[player.uniqueId]
+            if (target != lastTarget) {
+              lastTarget?.isGlowing = false
+
+              target.isGlowing = true
+              glowingTarget[player.uniqueId] = target
+            }
+          }
+        }
+        .runTaskTimer(EntityPose.INSTANCE, 0L, 2L) // 0Lにすることで、設置直後から判定を開始
+  }
 }
--- plugins/EntityPose/src/main/kotlin/me/ankokunsan/entityPose/EntityPose.kt
+++ plugins/EntityPose/src/main/kotlin/me/ankokunsan/entityPose/EntityPose.kt
@@ -14,61 +14,56 @@ import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.scoreboard.NameTagVisibility
 
 fun isEntiStick(item: ItemStack?): Boolean {
-    if (item == null || item.type.isAir) return false
-    if (!item.hasItemMeta()) return false
-    val meta = item.itemMeta ?: return false
-    return meta.persistentDataContainer.has(
-        EntityPose.ENTITY_STICK_KEY,
-        PersistentDataType.BYTE
-    )
+  if (item == null || item.type.isAir) return false
+  if (!item.hasItemMeta()) return false
+  val meta = item.itemMeta ?: return false
+  return meta.persistentDataContainer.has(EntityPose.ENTITY_STICK_KEY, PersistentDataType.BYTE)
 }
 
 class EntityPose : JavaPlugin() {
 
-    companion object {
-        lateinit var ENTITY_STICK_KEY: NamespacedKey
-        lateinit var GUI_KEY: NamespacedKey
-        lateinit var WOLF_KEY: NamespacedKey
-        lateinit var SIZE_KEY: NamespacedKey
-        lateinit var CAT_KEY: NamespacedKey
-        lateinit var RABBIT_KEY: NamespacedKey
-        lateinit var KAKUDO_KEY: NamespacedKey
-        lateinit var ZAHYO_KEY: NamespacedKey
-        lateinit var INSTANCE: EntityPose
-            private set
+  companion object {
+    lateinit var ENTITY_STICK_KEY: NamespacedKey
+    lateinit var GUI_KEY: NamespacedKey
+    lateinit var WOLF_KEY: NamespacedKey
+    lateinit var SIZE_KEY: NamespacedKey
+    lateinit var CAT_KEY: NamespacedKey
+    lateinit var RABBIT_KEY: NamespacedKey
+    lateinit var KAKUDO_KEY: NamespacedKey
+    lateinit var ZAHYO_KEY: NamespacedKey
+    lateinit var INSTANCE: EntityPose
+      private set
+  }
 
-    }
+  @Suppress("DEPRECATION")
+  override fun onEnable() {
+    INSTANCE = this
+    ENTITY_STICK_KEY = NamespacedKey(this, "entity_stick")
+    GUI_KEY = NamespacedKey(this, "gui_action")
+    WOLF_KEY = NamespacedKey(this, "wolf_spawn")
+    SIZE_KEY = NamespacedKey(this, "size_spawn")
+    CAT_KEY = NamespacedKey(this, "cat_spawn")
+    RABBIT_KEY = NamespacedKey(this, "rabbit_spawn")
+    KAKUDO_KEY = NamespacedKey(this, "angle_set")
+    ZAHYO_KEY = NamespacedKey(this, "zahyo_set")
 
-    @Suppress("DEPRECATION")
-    override fun onEnable() {
-        INSTANCE = this
-        ENTITY_STICK_KEY = NamespacedKey(this, "entity_stick")
-        GUI_KEY = NamespacedKey(this, "gui_action")
-        WOLF_KEY = NamespacedKey(this, "wolf_spawn")
-        SIZE_KEY = NamespacedKey(this, "size_spawn")
-        CAT_KEY = NamespacedKey(this, "cat_spawn")
-        RABBIT_KEY = NamespacedKey(this, "rabbit_spawn")
-        KAKUDO_KEY = NamespacedKey(this, "angle_set")
-        ZAHYO_KEY = NamespacedKey(this, "zahyo_set")
+    Bou.create()
+    getCommand("entitystick")?.setExecutor(Boucommand())
+    getCommand("entityinfo")?.setExecutor(Entityinfo())
+    getCommand("kakudo")?.setExecutor(KakudoCommand())
+    getCommand("zahyo")?.setExecutor(ZahyoCommand())
 
-        Bou.create()
-        getCommand("entitystick")?.setExecutor(Boucommand())
-        getCommand("entityinfo")?.setExecutor(Entityinfo())
-        getCommand("kakudo")?.setExecutor(KakudoCommand())
-        getCommand("zahyo")?.setExecutor(ZahyoCommand())
+    val board = Bukkit.getScoreboardManager()!!.mainScoreboard
 
-        val board = Bukkit.getScoreboardManager()!!.mainScoreboard
+    val team =
+        board.getTeam("animal_things_hide_name") ?: board.registerNewTeam("animal_things_hide_name")
 
-        val team = board.getTeam("animal_things_hide_name")
-            ?: board.registerNewTeam("animal_things_hide_name")
+    team.nameTagVisibility = NameTagVisibility.NEVER
+    server.pluginManager.registerEvents(EntityClick(), this)
+    server.pluginManager.registerEvents(GUIClick(), this)
+  }
 
-        team.nameTagVisibility = NameTagVisibility.NEVER
-        server.pluginManager.registerEvents(EntityClick(), this)
-        server.pluginManager.registerEvents(GUIClick(), this)
-
-    }
-
-    override fun onDisable() {
-        logger.info("EntityPose disabled")
-    }
+  override fun onDisable() {
+    logger.info("EntityPose disabled")
+  }
 }
