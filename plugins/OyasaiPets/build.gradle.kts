e67e30db9ada2319da05a9f92805a667835f8580
marzipan99 <0721shotaro@gmail.com>
2026-03-10 01:32:43 +0900

    リファクタリング実行

--- plugins/OyasaiPets/build.gradle.kts
+++ plugins/OyasaiPets/build.gradle.kts
@@ -1,6 +1,5 @@
 dependencies {
   compileOnly(libs.purpur.api)
-  compileOnly(project(":plugins:SocialLikes3"))
   compileOnly(files("libs/tokenmanager.jar"))
   implementation(libs.kotlin.stdlib)
 }
