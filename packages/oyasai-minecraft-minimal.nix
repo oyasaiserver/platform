c93226b22f0cd8397dff7c95690a02417b2f8e78
Shun Ueda <me@shu.nu>
2026-03-15 17:54:31 -0400

    chore: cleanup

--- packages/oyasai-minecraft-minimal.nix
+++ packages/oyasai-minecraft-minimal.nix
@@ -1,14 +1,10 @@
 { oyasaiPurpur, oyasai-plugin-registry }:
 
 oyasaiPurpur rec {
-  name = "oyasai-minecraft-marzipan";
+  name = "oyasai-minecraft-minimal";
   version = "1.21.8";
 
   directory = "local";
-
-  port = 8000;
-
-  cleanPlugins = false;
 
   # see packages/oyasai-plugins-registry/data.json
   plugins = with (oyasai-plugin-registry.forVersion version); [
