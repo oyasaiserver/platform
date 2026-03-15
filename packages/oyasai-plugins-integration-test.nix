20c351eb754d72b61a7cee2dcf31a7502878135c
Shun Ueda <me@shu.nu>
2026-03-15 14:36:28 -0400

    wip

---
+++ packages/oyasai-plugins-integration-test.nix
@@ -0,0 +1,43 @@
+{ testers, inputs }:
+
+testers.runNixOSTest (
+  { lib, ... }:
+  let
+    machine = "machine";
+    name = "minecraft-main";
+  in
+  {
+    name = "plugins-integration-test";
+    nodes.${machine} =
+      { config, ... }:
+      let
+        inherit (config.oyasai.scope) oyasai-minecraft-main;
+      in
+      {
+        imports = [
+          ../nix/nixos/oyasai-scope.nix
+          { _module.args = { inherit inputs; }; }
+        ];
+        virtualisation.oci-containers.backend = "docker";
+        systemd.services.${name} = {
+          serviceConfig = {
+            Type = "oneshot";
+            Restart = "no";
+            RemainAfterExit = "yes";
+            ExecStart = lib.getExe oyasai-minecraft-main;
+          };
+          enable = true;
+          wants = [ "multi-user.target" ];
+        };
+      };
+
+    testScript = ''
+      ${machine}.wait_for_unit("default.target")
+      ${machine}.systemctl("start --no-block ${name}.service")
+      ${machine}.wait_for_unit("${name}.service")
+    '';
+
+    # NOMERGE
+    # meta.platforms = lib.platforms.linux;
+  }
+)
