54247d7f2a6fe224e020c16ca1bb9e685597e3ab
Shun Ueda <me@shu.nu>
2026-04-13 16:14:12 -0400

    refactor(nix-cache-action): cleanup unused functions (#588)

--- packages/nix-cache-action/src/common.ts
+++ packages/nix-cache-action/src/common.ts
@@ -1,17 +1,20 @@
 import { readdirSync } from "node:fs";
 import { join } from "node:path";
-import { tmpdir } from "node:os";
 
+/* _Technically_ customizable */
 const NIX_STORE_PATH = "/nix/store";
 
 /*
  * Temporary file to save the snapshot of nix-store, that can be compared after
  * the build to take the diff in the nix-store.
  */
-export const STORE_SNAPSHOT_PATH = join(tmpdir(), "nix-store-pre-build");
+export const NIX_STORE_SNAPSHOT_PATH = ".nix-store-snapshot";
 
 /*
  * Inspired by: https://github.com/cachix/cachix-action/blob/a593539ec5b1ba1eb95f89a396efd45ca2cdaf5d/dist/list-nix-store.sh
+ *
+ * Node.js is a massive overkill for this, but the cost of spawning a
+ * child process is much more complicated, dangerous, and slow.
  */
 export function listNixStore(): readonly string[] {
   const skip = [".drv", ".drv.chroot", ".check", ".lock"];
--- packages/nix-s3-cache-action/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { writeFileSync } from "node:fs";
-import { EOL } from "node:os";
-import { listNixStore, STORE_SNAPSHOT_PATH } from "./common.ts";
-
-function main() {
-  writeFileSync(STORE_SNAPSHOT_PATH, listNixStore().join(EOL));
-}
-
-main();
