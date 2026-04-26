1c9e4f5addb83577bc6898d2f14aab3f2a994627
Shun Ueda <me@shu.nu>
2026-04-26 17:23:22 -0400

    feat: github management via cdktf

--- packages/oyasai-cdktf/src/stacks/common-internal.ts
+++ packages/oyasai-cdktf/src/stacks/common-internal.ts
@@ -5,6 +5,10 @@ import { CloudflareProvider } from "@oyasaiserver/cdktf-providers/cloudflare/pro
 import { R2Bucket } from "@oyasaiserver/cdktf-providers/cloudflare/r2-bucket";
 import type { CommonInfra } from "./common-infra.ts";
 import { R2CustomDomain } from "@oyasaiserver/cdktf-providers/cloudflare/r2-custom-domain";
+import { Repository } from "@oyasaiserver/cdktf-providers/github/repository";
+import { BranchProtection } from "@oyasaiserver/cdktf-providers/github/branch-protection";
+import { BranchDefault } from "@oyasaiserver/cdktf-providers/github/branch-default";
+import { RepositoryRuleset } from "@oyasaiserver/cdktf-providers/github/repository-ruleset";
 
 type Props = {
   commonInfra: CommonInfra;
@@ -19,11 +23,88 @@ export class CommonInternal extends OyasaiTerraformStack {
     this.createCloudBackend();
 
     new GithubProvider(this, "github-provider", {
+      // Required for app auth
       owner: "oyasaiserver",
-
+      // Must pass empty object for app auth
       // @ts-expect-error https://github.com/hashicorp/terraform-plugin-sdk/issues/142
       appAuth: {},
     });
+
+    const platformRepository = new Repository(
+      this,
+      this.t("platform-repository"),
+      {
+        name: "platform",
+        description: "OyasaiServer monorepo",
+        visibility: "public",
+        // Portals
+        hasIssues: true,
+        hasDiscussions: false,
+        hasProjects: false,
+        hasWiki: false,
+        // PR commit config
+        allowMergeCommit: false,
+        allowSquashMerge: true,
+        allowRebaseMerge: false,
+        allowAutoMerge: true,
+        squashMergeCommitTitle: "PR_TITLE",
+        squashMergeCommitMessage: "PR_BODY",
+        deleteBranchOnMerge: true,
+        allowUpdateBranch: true,
+        // DANGER: never destroy the repository.
+        lifecycle: {
+          preventDestroy: true,
+        },
+      },
+    );
+
+    new BranchDefault(
+      this,
+      this.t("platform-default-branch"),
+      {
+        repository: platformRepository.name,
+        branch: "master",
+        // DANGER: never destroy the default branch.
+        lifecycle: {
+          preventDestroy: true,
+        },
+      },
+    );
+
+    new RepositoryRuleset(this, this.t("platform-protect-master-repository-ruleset"), {
+      enforcement: "active",
+      name: "protect-master",
+      rules: {
+        pullRequest: {
+          allowedMergeMethods: ["squash"],
+          requiredReviewThreadResolution: true,
+          // Allow merge without an approval, unless protected by CODEOWNER
+          requireCodeOwnerReview: true,
+          requiredApprovingReviewCount: 0,
+        },
+        deletion: true,
+        requiredStatusChecks: {
+          requiredCheck: [
+            "nocommit",
+            "merging-into-master",
+            "pr-title",
+            "nix-flake-health",
+            "nix-flake-check (macos-latest)",
+            "nix-flake-check (ubuntu-latest)",
+            "nix-devshell (macos-latest)",
+            "nix-devshell (ubuntu-latest)"
+          ].map(context => ({context}))
+        }
+      },
+      target: "branch",
+      conditions: {
+        refName: {
+          exclude: [],
+          include: ["~DEFAULT_BRANCH"]
+        }
+      },
+      repository: platformRepository.name
+    })
 
     new CloudflareProvider(this, this.t("cloudflare-provider"));
 
