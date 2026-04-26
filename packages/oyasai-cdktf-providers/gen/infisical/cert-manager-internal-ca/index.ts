ed871a73d6116187d3b16cfd95dba4c926d71c56
Shun Ueda <me@shu.nu>
2026-04-26 11:17:36 -0400

    Revert "build: codegen built providers (#612)"
    This reverts commit 1591b91c8c8e8008cce4ef238968fe89b4206a84.

---
+++ packages/oyasai-cdktf-providers/gen/infisical/cert-manager-internal-ca/index.ts
@@ -0,0 +1,410 @@
+// https://registry.terraform.io/providers/infisical/infisical/0.16.18/docs/resources/cert_manager_internal_ca
+// generated from terraform resource schema
+
+import { Construct } from 'constructs';
+import * as cdktf from 'cdktf';
+
+// Configuration
+
+export interface CertManagerInternalCaConfig extends cdktf.TerraformMetaArguments {
+  /**
+  * The common name (CN) of the CA certificate
+  *
+  * Docs at Terraform Registry: {@link https://registry.terraform.io/providers/infisical/infisical/0.16.18/docs/resources/cert_manager_internal_ca#common_name CertManagerInternalCa#common_name}
+  */
+  readonly commonName?: string;
+  /**
+  * The country (C) of the CA certificate
+  *
+  * Docs at Terraform Registry: {@link https://registry.terraform.io/providers/infisical/infisical/0.16.18/docs/resources/cert_manager_internal_ca#country CertManagerInternalCa#country}
+  */
+  readonly country?: string;
+  /**
+  * The key algorithm for the CA. Supported values: RSA_2048, RSA_3072, RSA_4096, EC_prime256v1, EC_secp384r1, EC_secp521r1. Defaults to 'RSA_2048'.
+  *
+  * Docs at Terraform Registry: {@link https://registry.terraform.io/providers/infisical/infisical/0.16.18/docs/resources/cert_manager_internal_ca#key_algorithm CertManagerInternalCa#key_algorithm}
+  */
+  readonly keyAlgorithm?: string;
+  /**
+  * The locality (L) of the CA certificate
+  *
+  * Docs at Terraform Registry: {@link https://registry.terraform.io/providers/infisical/infisical/0.16.18/docs/resources/cert_manager_internal_ca#locality CertManagerInternalCa#locality}
+  */
+  readonly locality?: string;
+  /**
+  * The name of the CA
+  *
+  * Docs at Terraform Registry: {@link https://registry.terraform.io/providers/infisical/infisical/0.16.18/docs/resources/cert_manager_internal_ca#name CertManagerInternalCa#name}
+  */
+  readonly name: string;
+  /**
+  * The organization (O) of the CA certificate
+  *
+  * Docs at Terraform Registry: {@link https://registry.terraform.io/providers/infisical/infisical/0.16.18/docs/resources/cert_manager_internal_ca#organization CertManagerInternalCa#organization}
+  */
+  readonly organization?: string;
+  /**
+  * The organizational unit (OU) of the CA certificate
+  *
+  * Docs at Terraform Registry: {@link https://registry.terraform.io/providers/infisical/infisical/0.16.18/docs/resources/cert_manager_internal_ca#ou CertManagerInternalCa#ou}
+  */
+  readonly ou?: string;
+  /**
+  * The slug of the cert-manager project
+  *
+  * Docs at Terraform Registry: {@link https://registry.terraform.io/providers/infisical/infisical/0.16.18/docs/resources/cert_manager_internal_ca#project_slug CertManagerInternalCa#project_slug}
+  */
+  readonly projectSlug: string;
+  /**
+  * The state/province (ST) of the CA certificate
+  *
+  * Docs at Terraform Registry: {@link https://registry.terraform.io/providers/infisical/infisical/0.16.18/docs/resources/cert_manager_internal_ca#province CertManagerInternalCa#province}
+  */
+  readonly province?: string;
+  /**
+  * The status of the CA. Supported values: active, disabled. Defaults to 'active'.
+  *
+  * Docs at Terraform Registry: {@link https://registry.terraform.io/providers/infisical/infisical/0.16.18/docs/resources/cert_manager_internal_ca#status CertManagerInternalCa#status}
+  */
+  readonly status?: string;
+  /**
+  * The type of the CA. Supported values: root, intermediate
+  *
+  * Docs at Terraform Registry: {@link https://registry.terraform.io/providers/infisical/infisical/0.16.18/docs/resources/cert_manager_internal_ca#type CertManagerInternalCa#type}
+  */
+  readonly type: string;
+}
+
+/**
+* Represents a {@link https://registry.terraform.io/providers/infisical/infisical/0.16.18/docs/resources/cert_manager_internal_ca infisical_cert_manager_internal_ca}
+*/
+export class CertManagerInternalCa extends cdktf.TerraformResource {
+
+  // =================
+  // STATIC PROPERTIES
+  // =================
+  public static readonly tfResourceType = "infisical_cert_manager_internal_ca";
+
+  // ==============
+  // STATIC Methods
+  // ==============
+  /**
+  * Generates CDKTF code for importing a CertManagerInternalCa resource upon running "cdktf plan <stack-name>"
+  * @param scope The scope in which to define this construct
+  * @param importToId The construct id used in the generated config for the CertManagerInternalCa to import
+  * @param importFromId The id of the existing CertManagerInternalCa that should be imported. Refer to the {@link https://registry.terraform.io/providers/infisical/infisical/0.16.18/docs/resources/cert_manager_internal_ca#import import section} in the documentation of this resource for the id to use
+  * @param provider? Optional instance of the provider where the CertManagerInternalCa to import is found
+  */
+  public static generateConfigForImport(scope: Construct, importToId: string, importFromId: string, provider?: cdktf.TerraformProvider) {
+        return new cdktf.ImportableResource(scope, importToId, { terraformResourceType: "infisical_cert_manager_internal_ca", importId: importFromId, provider });
+      }
+
+  // ===========
+  // INITIALIZER
+  // ===========
+
+  /**
+  * Create a new {@link https://registry.terraform.io/providers/infisical/infisical/0.16.18/docs/resources/cert_manager_internal_ca infisical_cert_manager_internal_ca} Resource
+  *
+  * @param scope The scope in which to define this construct
+  * @param id The scoped construct ID. Must be unique amongst siblings in the same scope
+  * @param options CertManagerInternalCaConfig
+  */
+  public constructor(scope: Construct, id: string, config: CertManagerInternalCaConfig) {
+    super(scope, id, {
+      terraformResourceType: 'infisical_cert_manager_internal_ca',
+      terraformGeneratorMetadata: {
+        providerName: 'infisical',
+        providerVersion: '0.16.18'
+      },
+      provider: config.provider,
+      dependsOn: config.dependsOn,
+      count: config.count,
+      lifecycle: config.lifecycle,
+      provisioners: config.provisioners,
+      connection: config.connection,
+      forEach: config.forEach
+    });
+    this._commonName = config.commonName;
+    this._country = config.country;
+    this._keyAlgorithm = config.keyAlgorithm;
+    this._locality = config.locality;
+    this._name = config.name;
+    this._organization = config.organization;
+    this._ou = config.ou;
+    this._projectSlug = config.projectSlug;
+    this._province = config.province;
+    this._status = config.status;
+    this._type = config.type;
+  }
+
+  // ==========
+  // ATTRIBUTES
+  // ==========
+
+  // common_name - computed: false, optional: true, required: false
+  private _commonName?: string; 
+  public get commonName() {
+    return this.getStringAttribute('common_name');
+  }
+  public set commonName(value: string) {
+    this._commonName = value;
+  }
+  public resetCommonName() {
+    this._commonName = undefined;
+  }
+  // Temporarily expose input value. Use with caution.
+  public get commonNameInput() {
+    return this._commonName;
+  }
+
+  // country - computed: false, optional: true, required: false
+  private _country?: string; 
+  public get country() {
+    return this.getStringAttribute('country');
+  }
+  public set country(value: string) {
+    this._country = value;
+  }
+  public resetCountry() {
+    this._country = undefined;
+  }
+  // Temporarily expose input value. Use with caution.
+  public get countryInput() {
+    return this._country;
+  }
+
+  // id - computed: true, optional: false, required: false
+  public get id() {
+    return this.getStringAttribute('id');
+  }
+
+  // key_algorithm - computed: true, optional: true, required: false
+  private _keyAlgorithm?: string; 
+  public get keyAlgorithm() {
+    return this.getStringAttribute('key_algorithm');
+  }
+  public set keyAlgorithm(value: string) {
+    this._keyAlgorithm = value;
+  }
+  public resetKeyAlgorithm() {
+    this._keyAlgorithm = undefined;
+  }
+  // Temporarily expose input value. Use with caution.
+  public get keyAlgorithmInput() {
+    return this._keyAlgorithm;
+  }
+
+  // locality - computed: false, optional: true, required: false
+  private _locality?: string; 
+  public get locality() {
+    return this.getStringAttribute('locality');
+  }
+  public set locality(value: string) {
+    this._locality = value;
+  }
+  public resetLocality() {
+    this._locality = undefined;
+  }
+  // Temporarily expose input value. Use with caution.
+  public get localityInput() {
+    return this._locality;
+  }
+
+  // name - computed: false, optional: false, required: true
+  private _name?: string; 
+  public get name() {
+    return this.getStringAttribute('name');
+  }
+  public set name(value: string) {
+    this._name = value;
+  }
+  // Temporarily expose input value. Use with caution.
+  public get nameInput() {
+    return this._name;
+  }
+
+  // organization - computed: false, optional: true, required: false
+  private _organization?: string; 
+  public get organization() {
+    return this.getStringAttribute('organization');
+  }
+  public set organization(value: string) {
+    this._organization = value;
+  }
+  public resetOrganization() {
+    this._organization = undefined;
+  }
+  // Temporarily expose input value. Use with caution.
+  public get organizationInput() {
+    return this._organization;
+  }
+
+  // ou - computed: false, optional: true, required: false
+  private _ou?: string; 
+  public get ou() {
+    return this.getStringAttribute('ou');
+  }
+  public set ou(value: string) {
+    this._ou = value;
+  }
+  public resetOu() {
+    this._ou = undefined;
+  }
+  // Temporarily expose input value. Use with caution.
+  public get ouInput() {
+    return this._ou;
+  }
+
+  // project_slug - computed: false, optional: false, required: true
+  private _projectSlug?: string; 
+  public get projectSlug() {
+    return this.getStringAttribute('project_slug');
+  }
+  public set projectSlug(value: string) {
+    this._projectSlug = value;
+  }
+  // Temporarily expose input value. Use with caution.
+  public get projectSlugInput() {
+    return this._projectSlug;
+  }
+
+  // province - computed: false, optional: true, required: false
+  private _province?: string; 
+  public get province() {
+    return this.getStringAttribute('province');
+  }
+  public set province(value: string) {
+    this._province = value;
+  }
+  public resetProvince() {
+    this._province = undefined;
+  }
+  // Temporarily expose input value. Use with caution.
+  public get provinceInput() {
+    return this._province;
+  }
+
+  // status - computed: true, optional: true, required: false
+  private _status?: string; 
+  public get status() {
+    return this.getStringAttribute('status');
+  }
+  public set status(value: string) {
+    this._status = value;
+  }
+  public resetStatus() {
+    this._status = undefined;
+  }
+  // Temporarily expose input value. Use with caution.
+  public get statusInput() {
+    return this._status;
+  }
+
+  // type - computed: false, optional: false, required: true
+  private _type?: string; 
+  public get type() {
+    return this.getStringAttribute('type');
+  }
+  public set type(value: string) {
+    this._type = value;
+  }
+  // Temporarily expose input value. Use with caution.
+  public get typeInput() {
+    return this._type;
+  }
+
+  // =========
+  // SYNTHESIS
+  // =========
+
+  protected synthesizeAttributes(): { [name: string]: any } {
+    return {
+      common_name: cdktf.stringToTerraform(this._commonName),
+      country: cdktf.stringToTerraform(this._country),
+      key_algorithm: cdktf.stringToTerraform(this._keyAlgorithm),
+      locality: cdktf.stringToTerraform(this._locality),
+      name: cdktf.stringToTerraform(this._name),
+      organization: cdktf.stringToTerraform(this._organization),
+      ou: cdktf.stringToTerraform(this._ou),
+      project_slug: cdktf.stringToTerraform(this._projectSlug),
+      province: cdktf.stringToTerraform(this._province),
+      status: cdktf.stringToTerraform(this._status),
+      type: cdktf.stringToTerraform(this._type),
+    };
+  }
+
+  protected synthesizeHclAttributes(): { [name: string]: any } {
+    const attrs = {
+      common_name: {
+        value: cdktf.stringToHclTerraform(this._commonName),
+        isBlock: false,
+        type: "simple",
+        storageClassType: "string",
+      },
+      country: {
+        value: cdktf.stringToHclTerraform(this._country),
+        isBlock: false,
+        type: "simple",
+        storageClassType: "string",
+      },
+      key_algorithm: {
+        value: cdktf.stringToHclTerraform(this._keyAlgorithm),
+        isBlock: false,
+        type: "simple",
+        storageClassType: "string",
+      },
+      locality: {
+        value: cdktf.stringToHclTerraform(this._locality),
+        isBlock: false,
+        type: "simple",
+        storageClassType: "string",
+      },
+      name: {
+        value: cdktf.stringToHclTerraform(this._name),
+        isBlock: false,
+        type: "simple",
+        storageClassType: "string",
+      },
+      organization: {
+        value: cdktf.stringToHclTerraform(this._organization),
+        isBlock: false,
+        type: "simple",
+        storageClassType: "string",
+      },
+      ou: {
+        value: cdktf.stringToHclTerraform(this._ou),
+        isBlock: false,
+        type: "simple",
+        storageClassType: "string",
+      },
+      project_slug: {
+        value: cdktf.stringToHclTerraform(this._projectSlug),
+        isBlock: false,
+        type: "simple",
+        storageClassType: "string",
+      },
+      province: {
+        value: cdktf.stringToHclTerraform(this._province),
+        isBlock: false,
+        type: "simple",
+        storageClassType: "string",
+      },
+      status: {
+        value: cdktf.stringToHclTerraform(this._status),
+        isBlock: false,
+        type: "simple",
+        storageClassType: "string",
+      },
+      type: {
+        value: cdktf.stringToHclTerraform(this._type),
+        isBlock: false,
+        type: "simple",
+        storageClassType: "string",
+      },
+    };
+
+    // remove undefined attributes
+    return Object.fromEntries(Object.entries(attrs).filter(([_, value]) => value !== undefined && value.value !== undefined ))
+  }
+}
---
+++ packages/oyasai-cdktf-providers/gen/infisical/dynamic-secret-aws-iam/README.md
@@ -0,0 +1,3 @@
+# `infisical_dynamic_secret_aws_iam`
+
+Refer to the Terraform Registry for docs: [`infisical_dynamic_secret_aws_iam`](https://registry.terraform.io/providers/infisical/infisical/0.16.18/docs/resources/dynamic_secret_aws_iam).
