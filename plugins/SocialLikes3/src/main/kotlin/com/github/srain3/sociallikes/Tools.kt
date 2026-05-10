d15b1ea2e111840eed3f716a1c0991b1b8eda82d
takucan <takucancrafter@gmail.com>
2026-05-11 01:05:23 +0900

    slupdate region

--- plugins/SocialLikes3/src/main/kotlin/com/github/srain3/sociallikes/Tools.kt
+++ plugins/SocialLikes3/src/main/kotlin/com/github/srain3/sociallikes/Tools.kt
@@ -1,20 +1,34 @@
 package com.github.srain3.sociallikes
 
 import com.fren_gor.ultimateAdvancementAPI.UltimateAdvancementAPI
+import com.github.srain3.sociallikes.Events.idKey
+import com.github.srain3.sociallikes.command.SLUpdate
+import com.github.srain3.sociallikes.datas.Data
+import com.github.srain3.sociallikes.datas.SLData
+import com.github.srain3.sociallikes.discord.SLDiscord
 import java.io.File
 import me.realized.tokenmanager.api.TokenManager
 import net.luckperms.api.LuckPermsProvider
 import org.bukkit.Bukkit
 import org.bukkit.ChatColor
+import org.bukkit.Material
+import org.bukkit.block.BlockState
+import org.bukkit.block.Sign
+import org.bukkit.block.sign.Side
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemFlag
 import org.bukkit.inventory.ItemStack
+import org.bukkit.persistence.PersistentDataType
 import org.bukkit.plugin.java.JavaPlugin.getPlugin
+import kotlin.collections.set
 
 object Tools {
   /** JavaPluginクラス(Main) */
   val plugin by lazy { getPlugin(SocialLikes::class.java) }
-
+  /** (SocialLikes)←これの判定用 */
+  val slSignRegex = Regex("""\(SocialLikes\)""")
+  /** \[SocialLikes]←これの判定用(エルちゃん(カイザー)製SL看板用) */
+  val oldSlSignRegex = Regex("""\[SocialLikes]""")
   /** LuckPermsクラス(API) */
   val lp by lazy { LuckPermsProvider.get() }
 
@@ -109,6 +123,90 @@ object Tools {
     } else {
       null
     }
+  }
+
+  fun isSLSign(block: BlockState): Boolean {
+    if (block !is Sign) return false
+    val unColorFrontL0 = block.getSide(Side.FRONT).getLine(0).unColor()
+    return slSignRegex.containsMatchIn(unColorFrontL0)
+  }
+
+  fun isLegacySLSign(block: BlockState): Boolean {
+    if (block !is Sign) return false
+    val unColorFrontL0 = block.getSide(Side.FRONT).getLine(0).unColor()
+    return oldSlSignRegex.containsMatchIn(unColorFrontL0)
+  }
+
+  fun updateSLSign(currentSLData: SLData, block: BlockState){
+    val newData =
+      SLData(
+        currentSLData.id,
+        block.location,
+        currentSLData.time,
+        currentSLData.owner,
+        currentSLData.title,
+        currentSLData.likes,
+        currentSLData.likesWithTimestamp,
+        currentSLData.check,
+        currentSLData.comment,
+        block.world.name,
+        currentSLData.discordTextID,
+      )
+    Data.delID(currentSLData, true)
+    Data.save(newData)
+    if (currentSLData.loc.world != null) {
+      val state = currentSLData.loc.block.state
+      if (state is Sign) {
+        state.block.blockData = Material.AIR.createBlockData()
+        state.update()
+      }
+    }
+    // Discordへ反映
+    SLDiscord.changeSLDataToMsg(newData)
+  }
+
+  fun updateLegacySLSign(currentSLData: SLData, block: BlockState) {
+    if (block !is Sign) return
+    var id =
+      block
+        .getSide(Side.FRONT)
+        .line(0)
+        .style()
+        .color()
+        ?.asHexString()
+        ?.substring(1)
+        ?.toIntOrNull(16) ?: return
+    id = -id
+    val newData =
+      SLData(
+        currentSLData.id,
+        block.location,
+        currentSLData.time,
+        currentSLData.owner,
+        currentSLData.title,
+        currentSLData.likes,
+        currentSLData.likesWithTimestamp,
+        currentSLData.check,
+        currentSLData.comment,
+        block.world.name,
+        currentSLData.discordTextID,
+      )
+    Data.delID(currentSLData, true)
+    Data.save(newData)
+    // 看板の装飾
+    block.setLine(0, Tools.socialLikesLOGO)
+    block.setLine(1, "&a".color() + currentSLData.title)
+    block.setLine(2, "&f${Bukkit.getOfflinePlayer(currentSLData.owner).name}".color())
+    block.setLine(
+      3,
+      "&7Likes&8: &6${currentSLData.likes.count()}${if(currentSLData.check){" &e✓"}else{""}}".color(),
+    )
+
+    block.isWaxed = true
+    block.persistentDataContainer.set(idKey, PersistentDataType.INTEGER, id)
+    block.update()
+
+
   }
 
   /** AdvancementAPI */
--- plugins/SocialLikes3/src/main/kotlin/com/github/srain3/sociallikes/command/SLUpdate.kt
+++ plugins/SocialLikes3/src/main/kotlin/com/github/srain3/sociallikes/command/SLUpdate.kt
@@ -1,12 +1,28 @@
 package com.github.srain3.sociallikes.command
 
+import com.github.srain3.sociallikes.Events.idKey
 import com.github.srain3.sociallikes.Tools
 import com.github.srain3.sociallikes.Tools.color
+import com.github.srain3.sociallikes.Tools.isLegacySLSign
+import com.github.srain3.sociallikes.Tools.isSLSign
+import com.github.srain3.sociallikes.Tools.updateLegacySLSign
+import com.github.srain3.sociallikes.Tools.updateSLSign
+import com.github.srain3.sociallikes.datas.Data
+import com.sk89q.worldedit.WorldEdit
+import com.sk89q.worldedit.bukkit.BukkitAdapter
+import com.sk89q.worldedit.session.SessionOwner
+import com.sk89q.worldedit.util.formatting.text.TextComponent
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.event.ClickEvent
+import net.kyori.adventure.text.format.TextColor
+import org.bukkit.block.Sign
+import org.bukkit.block.sign.Side
 import java.util.*
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
+import org.bukkit.persistence.PersistentDataType
 import org.bukkit.scheduler.BukkitRunnable
 
 object SLUpdate : CommandExecutor {
@@ -20,7 +36,10 @@ object SLUpdate : CommandExecutor {
   ): Boolean {
     if (command.name != "slupdate") return false
     if (sender !is Player) return false
-
+    if (args.size > 1 && args[1] == "region"){
+      updateAllInRegion(sender)
+      return true
+    }
     sender.sendMessage(Tools.socialLikesLOGO + "&eアップデートモードに入りました".color())
     sender.sendMessage(Tools.socialLikesLOGO + "&e移動させたlike看板を右クリックで更新します(10秒後モード終了)".color())
     switch[sender.uniqueId] = true
@@ -38,5 +57,64 @@ object SLUpdate : CommandExecutor {
         .runTaskLater(Tools.plugin, 20 * 10)
 
     return true
+  }
+
+  fun updateAllInRegion(player: Player){
+    //FAWEから選択範囲のブロックリストを取得
+    val we = WorldEdit.getInstance()
+    if (we == null) {
+      player.sendMessage("WorldEdit読み込み失敗")
+      return
+    }
+
+    val actor = BukkitAdapter.adapt(player)
+    val localSession = we.sessionManager.get(actor as SessionOwner)
+
+    val clipboardHolder = localSession.existingClipboard ?: run {
+      actor.printError(TextComponent.of("Your clipboard is empty."))
+      return
+    }
+
+    val clipboard = clipboardHolder.getClipboards().firstOrNull() ?: run {
+      actor.printError(TextComponent.of("Your clipboard is empty."))
+      return
+    }
+    val region = clipboard.region
+    player.sendMessage("${region.width}x${region.height}x${region.length}")
+    region.forEach { blockVector3 ->
+      val block = player.world.getBlockAt(blockVector3.x(), blockVector3.y(), blockVector3.z())
+      if (block !is Sign) return
+      var id = block.persistentDataContainer.get(idKey, PersistentDataType.INTEGER) ?: return
+      if (isSLSign(block.state)){
+
+        val data = Data.getSLData(id) ?: return
+        updateSLSign(data, block)
+
+      } else if (isLegacySLSign(block.state)){
+
+        id =
+          block
+            .getSide(Side.FRONT)
+            .line(0)
+            .style()
+            .color()
+            ?.asHexString()
+            ?.substring(1)
+            ?.toIntOrNull(16) ?: return
+        id = -id
+        val data = Data.getSLData(id) ?: return
+        updateLegacySLSign(data, block.state)
+
+      } else return
+
+      val message = Component.text(Tools.socialLikesLOGO)
+      .append(Component.text("アップデートしました！ ").color(TextColor.color(0x55FF55)))
+      .append(Component.text("Location: ${block.world} / ${block.x}, ${block.y}, ${block.z}").color(TextColor.color(0xAAAAAA)))
+      .append(Component.text("ID: ${id}").color(TextColor.color(0xAAAAAA)))
+      .hoverEvent(Component.text("クリックで ${id} にテレポート"))
+      .clickEvent(
+        ClickEvent.runCommand("/minecraft:execute in ${block.world} run minecraft:tp ${block.x} ${block.y} ${block.z}"))
+      player.sendMessage(message)
+    }
   }
 }
