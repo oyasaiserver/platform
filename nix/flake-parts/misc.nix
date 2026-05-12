2e195bcd7910b70c8abcaca21eb7856b45222736
Shun Ueda <me@shu.nu>
2026-05-12 01:13:28 -0400

    revert: introduce dune2nix (#657)
    Reverts oyasaiserver/platform#651

--- nix/flake-parts/misc.nix
+++ nix/flake-parts/misc.nix
@@ -9,7 +9,6 @@
           config.allowUnfree = true;
           overlays = [
             inputs.nix-minecraft.overlay
-            inputs.dune2nix.overlays.dune
             (import ../oyasai-overlay.nix { inherit inputs; })
           ];
         };
