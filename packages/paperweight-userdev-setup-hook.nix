598d8932506f5123391cd85f312baf452f7f100b
Shun Ueda <me@shu.nu>
2026-05-15 10:25:10 -0400

    fix: bring back setup hook

---
+++ packages/paperweight-userdev-setup-hook.nix
@@ -0,0 +1,50 @@
+# Setup hook that pre-populates the paperweight-userdev vanillaServerDownloads cache
+# for every version in mojang-server-mappings, so the download task is skipped in the
+# Nix sandbox. Must run after gradleConfigurePhase sets GRADLE_USER_HOME, so it
+# registers to postConfigureHooks.
+{
+  lib,
+  makeSetupHook,
+  jq,
+  writeText,
+  vanillaServers,
+  fetchurl,
+  inputs,
+}:
+let
+  mojang-server-mappings = lib.importJSON "${inputs.nix-minecraft}/pkgs/neoforge-servers/game_locks.json";
+
+  script = writeText "paperweight-userdev-setup-hook.sh" ''
+    paperweightUserdevPopulateCache() {
+      ${lib.concatMapAttrsStringSep "\n" (
+        version:
+        { mappings }:
+        let
+          # Reimplementation of paperweight cache key
+          cacheKey = (builtins.hashString "sha256" (builtins.hashString "sha256" version));
+          vanillaServerJar = vanillaServers."vanilla-${lib.replaceString "." "_" version}";
+        in
+        ''
+          dir="$GRADLE_USER_HOME/caches/paperweight-userdev/v2/work/vanillaServerDownloads_${cacheKey}"
+
+          mkdir -p "$dir"
+
+          cp ${vanillaServerJar}/lib/minecraft/server.jar "$dir/vanillaServer.jar"
+          cp ${fetchurl mappings} "$dir/mojangServerMappings.txt"
+
+          serverJarHash=$(sha256sum "$dir/vanillaServer.jar" | cut -d' ' -f1)
+          mappingsHash=$(sha256sum "$dir/mojangServerMappings.txt" | cut -d' ' -f1)
+
+          jq -n --arg h1 "$serverJarHash" --arg h2 "$mappingsHash" \
+            '{ outputHashes: [$h1, $h2], lastUsed: 0 }' \
+            >"$dir/metadata.json"
+        ''
+      ) mojang-server-mappings}
+    }
+    postConfigureHooks+=(paperweightUserdevPopulateCache)
+  '';
+in
+makeSetupHook {
+  name = "paperweight-userdev-setup-hook";
+  propagatedBuildInputs = [ jq ];
+} script
