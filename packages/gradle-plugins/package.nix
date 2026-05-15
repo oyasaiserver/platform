d928429f52aa7432a5047ca7ac37b1befb4be8cb
Shun Ueda <me@shu.nu>
2026-05-15 14:49:20 -0400

    ci: non-nix development

--- packages/gradle-plugins/package.nix
+++ packages/gradle-plugins/package.nix
@@ -2,31 +2,9 @@
   runCommand,
   ktfmt,
   gradle2nix,
-  jdk,
-  writableTmpDirAsHomeHook
 }:
 
-runCommand "gradle-plugins" {
-  passthru.oyasai-tests.basic = runCommand "basic" {
-    nativeBuildInputs = [
-      jdk
-      writableTmpDirAsHomeHook
-    ];
-  }
-    ''
-      cd ${../..}
-
-      export GRADLE_USER_HOME=$(mktemp -d)
-
-      ls -la
-
-      java --version
-      ./gradlew --version
-
-      touch $out
-    ''
-  ;
-} ''
+runCommand "gradle-plugins" { } ''
   mkdir -p $out/ktfmt/share $out/gradle2nix
 
   cp ${ktfmt}/share/ktfmt/* $out/ktfmt/share
