ffa4e67189f526aeb0863602798d1096f4e019b6
Shun Ueda <me@shu.nu>
2026-05-25 21:53:38 -0400

    feat: axiom (#702)

---
+++ packages/oyasai-minecraft-axiom.nix
@@ -0,0 +1,37 @@
+{ oyasaiPurpur, oyasai-plugin-registry }:
+
+oyasaiPurpur rec {
+  name = "oyasai-minecraft-axiom";
+  version = "1.21.10";
+
+  properties = {
+    # keep-sorted start
+    enable-rcon = true;
+    force-gamemode = true;
+    gamemode = "creative";
+    online-mode = false; # handled by velocity
+    white-list = true;
+    # keep-sorted endt
+  };
+
+  paperConfig = {
+    proxies.velocity = {
+      enabled = true;
+      online-mode = true;
+    };
+  };
+
+  plugins = with oyasai-plugin-registry.forVersion version; [
+    # keep-sorted start
+    arceon
+    arceon-axiom
+    axiom-paper-plugin
+    ezedits
+    fastasyncvoxelsniper
+    fastasyncworldedit
+    floodgate
+    plugmanx
+    viaversion
+    # keep-sorted end
+  ];
+}
