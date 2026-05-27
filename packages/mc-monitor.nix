58c61e4dc8aab428877e893d77445c67e2dd6f2f
Shun Ueda <me@shu.nu>
2026-05-27 12:47:08 -0400

    build: update flake inputs (#710)

--- packages/mc-monitor.nix
+++ packages/mc-monitor.nix
@@ -4,7 +4,7 @@ buildGo126Module {
   pname = "mc-monitor";
   version = "0.16.5";
   src = inputs.mc-monitor;
-  vendorHash = "sha256-6E/hQu65Cj/vPG+SDHMwyHQOAlcNrJ6ZqflzYI71RXU=";
+  vendorHash = "sha256-Zc1KpKFQq4Q+eKj3V1d6uC4RQm7R4fd1XKqAwuqsEWk=";
 
   meta.mainProgram = "mc-monitor";
 }
