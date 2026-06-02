11b946ecffcbd299baab7b3fd6869e7d12431db8
takucan <takucancrafter@gmail.com>
2026-06-03 03:02:54 +0900

    pita

---
+++ plugins/OyasaiUtilities/src/main/kotlin/icu/oyasai/utilities/pita/Pita.kt
@@ -0,0 +1,106 @@
+package icu.oyasai.utilities.pita
+
+import icu.oyasai.utilities.YamlConfig
+import icu.oyasai.utilities.tpath.TeleportManager
+import java.util.UUID
+import org.bukkit.Bukkit
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerMoveEvent
+import org.bukkit.util.Vector
+
+object Pita : Listener, CommandExecutor {
+
+  private val enabledPlayers = mutableSetOf<UUID>()
+  private var defaultEnabled = false
+  private val config by lazy { YamlConfig("Pita/config.yml", true) }
+  private val dataFile by lazy { YamlConfig("Pita/data.yml") }
+
+  fun onEnable() {
+    // load config
+    config.saveDefault()
+    defaultEnabled = config.getBoolean("default-enabled", false)
+
+    // load data
+    loadData()
+  }
+
+  fun onDisable() {
+    saveData()
+  }
+
+  private fun loadData() {
+    dataFile.reload()
+    val list = dataFile.getStringList("enabled-players")
+    list.forEach { uuidString ->
+      try {
+        enabledPlayers.add(UUID.fromString(uuidString))
+      } catch (_: Exception) {}
+    }
+  }
+
+  private fun saveData() {
+    dataFile.set("enabled-players", enabledPlayers.map { it.toString() })
+    dataFile.save()
+  }
+
+  override fun onCommand(
+      sender: CommandSender,
+      command: Command,
+      label: String,
+      args: Array<out String>,
+  ): Boolean {
+    if (sender !is Player) {
+      sender.sendMessage("This command can only be used by players.")
+      return true
+    }
+
+    val uuid = sender.uniqueId
+    if (enabledPlayers.contains(uuid)) {
+      enabledPlayers.remove(uuid)
+      sender.sendMessage("§aPita Mode: §cOFF")
+      Bukkit.getLogger().info("${sender.name} has disabled Pita Mode.")
+    } else {
+      enabledPlayers.add(uuid)
+      sender.sendMessage("§aPita Mode: §bON (Pita!)")
+      Bukkit.getLogger().info("${sender.name} has enabled Pita Mode.")
+    }
+    return true
+  }
+
+  @EventHandler
+  fun onPlayerMove(event: PlayerMoveEvent) {
+    val player = event.player
+
+    if (!enabledPlayers.contains(player.uniqueId) && !defaultEnabled) return
+
+    if (!player.isFlying) return
+
+    val input = player.currentInput
+    val isMoving =
+        input.isForward ||
+            input.isBackward ||
+            input.isLeft ||
+            input.isRight ||
+            input.isJump ||
+            input.isSneak
+
+    if (!isMoving) {
+      val from = event.from
+      val to = event.to
+
+      if (from.x != to.x || from.y != to.y || from.z != to.z) {
+        TeleportManager.pushIgnore(player)
+
+        val newTo = from.clone()
+
+        event.to = newTo
+        player.velocity = Vector(0.0, 0.0, 0.0)
+      }
+    }
+  }
+}
