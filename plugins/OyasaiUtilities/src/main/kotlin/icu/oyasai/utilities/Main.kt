ef609c8361e406313715e9d995695eb2d92b6e83
takucan <80089466+takucan@users.noreply.github.com>
2026-06-03 07:23:13 +0900

    feat(oyasaiutilities): add pita mode (#716)

--- plugins/OyasaiUtilities/src/main/kotlin/icu/oyasai/utilities/Main.kt
+++ plugins/OyasaiUtilities/src/main/kotlin/icu/oyasai/utilities/Main.kt
@@ -7,6 +7,7 @@ import icu.oyasai.utilities.notnbt.NotNBTEvent
 import icu.oyasai.utilities.ore_reappears.OreReappears
 import icu.oyasai.utilities.oresmelter.OreSmelter
 import icu.oyasai.utilities.oresmelter.OreSmelterEvent
+import icu.oyasai.utilities.pita.Pita
 import icu.oyasai.utilities.timerbar.TimerBarEvent
 import icu.oyasai.utilities.timerbar.TimerCmd
 import icu.oyasai.utilities.timerbar.TimerObj
@@ -22,15 +23,18 @@ class Main : JavaPlugin() {
     server.pluginManager.registerEvents(OreSmelterEvent, this) // OreSmelterのイベント登録
     server.pluginManager.registerEvents(TimerBarEvent, this) // TimerBar用のイベント登録
     server.pluginManager.registerEvents(TeleportListener, this) // TPathのイベント登録
+    server.pluginManager.registerEvents(Pita, this) // Pitaのイベント
 
     server.getPluginCommand("oresmelter")?.setExecutor(OreSmelter) // OreSmelterのコマンド
     server.getPluginCommand("uuid")?.setExecutor(GetUUIDCmd) // GetUUIDのコマンド
     server.getPluginCommand("timerbar")?.setExecutor(TimerCmd) // TimerBarのコマンド
     server.getPluginCommand("back")?.setExecutor(BackForwardCmd) // back コマンド
     server.getPluginCommand("forward")?.setExecutor(BackForwardCmd) // forward コマンド
+    server.getPluginCommand("pita")?.setExecutor(Pita) // Pitaのコマンド
 
     OreReappears.onEnable() // OreReappearsの有効化
     AdminBP.onEnable()
+    Pita.onEnable() // Pitaの有効化
     OreSmelter.reloadConfig() // OreSmelterのコンフィグリロード
     CreativeManagement.onEnable()
   }
@@ -38,6 +42,7 @@ class Main : JavaPlugin() {
   override fun onDisable() {
     OreReappears.onDisable() // OreReappearsの無効化
     AdminBP.onDisable()
+    Pita.onDisable() // Pitaの無効化
     TimerObj.onDisable()
   }
 }
