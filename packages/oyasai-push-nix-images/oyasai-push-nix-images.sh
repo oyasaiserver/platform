65b2050a1694e1af68869fa8fda8f4ab88b39242
Shun Ueda <me@shu.nu>
2026-06-02 23:50:20 -0400

    push images

--- packages/oyasai-push-nix-images/oyasai-push-nix-images.sh
+++ packages/oyasai-push-nix-images/oyasai-push-nix-images.sh
@@ -4,12 +4,21 @@ set -euo pipefail
 
 images="$(mktemp)"
 
-trap 'rm -f $images' EXIT
+# shellcheck disable=SC2064
+trap "rm -f ${images@Q}" EXIT
 
-nix build --no-link --print-out-paths .#all-docker-derivations | xargs -r cat | while read -r name drv; do
-  if ! docker manifest inspect "$name" &>/dev/null; then
-    printf "%s\t%s^out\n" "$name" "$drv"
-  fi
-done >"$images"
-<"$images" cut -f 2 | xargs -r nix build --no-link --print-out-paths | xargs -r -n 1 docker load -i
-<"$images" cut -f 1 | xargs -r -n 1 docker push
+nix build --no-link --print-out-paths .#all-docker-derivations |
+  xargs -r cat |
+  while read -r name drv; do
+    if ! skopeo inspect --no-tags "docker://$name" &>/dev/null; then
+      printf "%s\t%s^out\n" "$name" "$drv"
+    fi
+  done >"$images"
+
+cat "$images"
+
+<"$images" cut -f 2 | xargs -r nix build --no-link --print-out-paths --keep-going
+while read -r name drv; do
+  out="$(nix build --no-link --print-out-paths "$drv")"
+  skopeo --insecure-policy copy "docker-archive:$out" "docker://$name"
+done <"$images"
--- packages/oyasai-push-nix-images/package.nix
+++ packages/oyasai-push-nix-images/package.nix
@@ -1,6 +1,14 @@
-{ writeShellApplication }:
+{
+  findutils,
+  skopeo,
+  writeShellApplication,
+}:
 
 writeShellApplication {
   name = "oyasai-push-nix-images";
+  runtimeInputs = [
+    findutils
+    skopeo
+  ];
   text = builtins.readFile ./oyasai-push-nix-images.sh;
 }
