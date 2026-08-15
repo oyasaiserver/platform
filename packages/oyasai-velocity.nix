014cbd25574dacc6721b6dd448b3a4f81475bc1f
Shun Ueda <git@shunueda.org>
2026-08-15 18:30:10 -0400

    chore: update max-players

--- packages/oyasai-velocity.nix
+++ packages/oyasai-velocity.nix
@@ -19,8 +19,9 @@ oyasaiVelocity {
 
   velocityConfig = {
     config-version = "2.8";
-    motd = "                 <aqua><bold>Oyasai<white>Server<reset>\n<bold>            建築勢は集合だ！建築！建築！建築！！！";
+    motd = "                 <aqua><bold>Oyasai<white>Server<reset>\n<bold>      建築勢は集合だ！建築！建築！建築！！！";
     player-info-forwarding-mode = "modern";
+    show-max-players = 114514;
 
     # Velocity tries to set default
     forced-hosts = { };
