24a065b39d9fb4c356c97c934f4b18edb629c3d1
takucan <80089466+takucan@users.noreply.github.com>
2026-08-24 04:07:35 +0900

    fix(oyasaichat): exclude net.kyori from velocity-api (#879)

--- plugins/OyasaiChat/build.gradle.kts
+++ plugins/OyasaiChat/build.gradle.kts
@@ -5,6 +5,6 @@ dependencies {
   compileOnly(libs.purpur.api)
   compileOnly(libs.discordsrv)
   compileOnly(libs.placeholderapi)
-  compileOnly(libs.velocity.api)
+  compileOnly(libs.velocity.api) { exclude(group = "net.kyori") }
   compileOnly(libs.vault.api)
 }
