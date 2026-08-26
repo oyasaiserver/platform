ecb0e53f758e0dcd58e588eb8716276874227ba8
Shun Ueda <git@shunueda.org>
2026-08-25 21:59:00 -0400

    update mcmonitor

--- packages/mc-monitor.nix
+++ packages/mc-monitor.nix
@@ -4,7 +4,7 @@ buildGo126Module {
   pname = "mc-monitor";
   version = "0.17.1";
   src = inputs.mc-monitor;
-  vendorHash = "sha256-4Cxt8S8jFp/d2G/S5vi0gZBVBTXVHxh+JoQSdTkToO4=";
+  vendorHash = "sha256-LWkvThn08cgWRZCYN46wY0hs53FSt3HZHex+R12mkbw=";
 
   meta.mainProgram = "mc-monitor";
 }
