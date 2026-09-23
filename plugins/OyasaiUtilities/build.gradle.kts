75e8c7be0ef47105d3cfb8e7148bfd662729e667
marzipan99 <0721shotaro@gmail.com>
2026-09-23 12:15:37 +0900

    feat(oyasaiutilities): add one-time Minepacks backpack migration

--- plugins/OyasaiUtilities/build.gradle.kts
+++ plugins/OyasaiUtilities/build.gradle.kts
@@ -1,8 +1,15 @@
 plugins { alias(libs.plugins.paperweight.userdev) }
 
+repositories {
+  maven("https://repo.pcgamingfreaks.at/repository/maven-everything/") {
+    content { includeGroup("at.pcgamingfreaks") }
+  }
+}
+
 dependencies {
   paperweightDevelopmentBundle(libs.purpur.dev.bundle)
   implementation(libs.sqlite.jdbc)
+  compileOnly("at.pcgamingfreaks:Minepacks-API:2.5.9") { isTransitive = false }
   compileOnly(libs.vault.api)
   compileOnly(libs.luckperms.api)
   compileOnly(project(":plugins:OyasaiToken"))
